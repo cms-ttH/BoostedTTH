6657184deca339fdd9fe5498e99c94eebeec14d7
Hannes Mildner <hannes.mildner@cern.ch>
2015-02-25 00:32:09 +0100

    rewrote config for fat jet producer: allow unscheduled, removed warnings, updated ele IDs, keep only miniAOD and boosted jet collections

--- BoostedProducer/test/boostedProducer_cfg.py
+++ BoostedProducer/test/boostedProducer_cfg.py
@@ -1,47 +1,26 @@
 import FWCore.ParameterSet.Config as cms
-from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
-
+# input
 process = cms.Process("p")
-
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000000000)
-)
-
 process.source = cms.Source("PoolSource",
-    secondaryFileNames = cms.untracked.vstring(),
-    #fileNames = cms.untracked.vstring('file:/nfs/dust/cms/user/shwillia/Test/ttbar_miniAODtest.root')
-    fileNames = cms.untracked.vstring('file:/nfs/dust/cms/user/shwillia/Test/ttH_Phys14_miniAOD.root')
+                            fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00C90EFC-3074-E411-A845-002590DB9262.root')
+                            #fileNames = cms.untracked.vstring('file:/storage/9/mildner/ttbar_phys14.root')
+                            #fileNames = cms.untracked.vstring('file:/nfs/dust/cms/user/shwillia/Test/ttbar_miniAODtest.root')
 )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5000) )
 
-
-process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration.EventContent.EventContent_cff')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
-process.load("Configuration.EventContent.EventContent_cff")
-process.GlobalTag.globaltag = 'PHYS14_25_V2::All'
+# messages
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.MessageLogger.suppressWarning = cms.untracked.vstring('ecalLaserCorrFilter','manystripclus53X','toomanystripclus53X')
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.options.allowUnscheduled = cms.untracked.bool(True)
 
-# unpack tracks and vertices
-process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
-process.unpackedTracksAndVertices = cms.EDProducer("PATTrackAndVertexUnpacker",
-    slimmedVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
-    slimmedSecondaryVertices = cms.InputTag("slimmedSecondaryVertices"),
-    packedCandidates = cms.InputTag("packedPFCandidates"),
-    additionalTracks = cms.InputTag("lostTracks")
-)
-
-#select isolated collections
+# select loose lepton collections
 process.selectedMuons = cms.EDFilter("CandPtrSelector", src = cms.InputTag("slimmedMuons"), cut = cms.string('''abs(eta)<2.5 && pt>10. &&
-(pfIsolationR04().sumChargedHadronPt+
-max(0.,pfIsolationR04().sumNeutralHadronEt+
-pfIsolationR04().sumPhotonEt-
-0.50*pfIsolationR04().sumPUPt))/pt < 0.20 &&
+(pfIsolationR03().sumChargedHadronPt+
+max(0.,pfIsolationR03().sumNeutralHadronEt+
+pfIsolationR03().sumPhotonEt-
+0.50*pfIsolationR03().sumPUPt))/pt < 0.20 &&
 (isPFMuon && (isGlobalMuon || isTrackerMuon) )'''))
 process.selectedElectrons = cms.EDFilter("CandPtrSelector", src = cms.InputTag("slimmedElectrons"), cut = cms.string('''abs(eta)<2.5 && pt>20. &&
 gsfTrack.isAvailable() &&
@@ -53,23 +32,29 @@ pfIsolationVariables().sumPhotonEt-
 
 #do projections
 process.pfCHS = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("fromPV"))
-process.pfNoMuonCHS = cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfCHS"), veto = cms.InputTag("selectedMuons"))
-process.pfNoElectronsCHS = cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfNoMuonCHS"), veto = cms.InputTag("selectedElectrons"))
+process.pfNoMuonCHS =  cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfCHS"), veto = cms.InputTag("selectedMuons"))
+process.pfNoElectronsCHS = cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfNoMuonCHS"), veto =  cms.InputTag("selectedElectrons"))
 
-# load subfilterjet algorithm
-process.load("BoostedTTH.BoostedProducer.SubjetsFilterjets_cfi")
+# make patJets 
+process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load('Configuration.StandardSequences.Geometry_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'PHYS14_25_V2::All'
 
-# load heptoptagger
-process.load("BoostedTTH.BoostedProducer.HEPTopJetParameters_cfi")
+# Fat jets
+from BoostedTTH.BoostedProducer.SubjetsFilterjets_cfi import CA12JetsCA3FilterjetsPF
+from BoostedTTH.BoostedProducer.HEPTopJetParameters_cfi import HEPTopJetsPF
+process.CA12JetsCA3FilterjetsPF=CA12JetsCA3FilterjetsPF.clone(src = 'pfNoElectronsCHS')
+process.CA12JetsCA3FilterjetsPF.jetPtMin=cms.double(180.)
+process.CA12JetsCA3FilterjetsPF.doAreaFastjet=cms.bool(False)
+process.HEPTopJetsPF=HEPTopJetsPF.clone(src = 'pfNoElectronsCHS')
+process.HEPTopJetsPF.jetPtMin=cms.double(180.)
+process.HEPTopJetsPF.doAreaFastjet=cms.bool(True)
 
-# use pfNoElectron
-process.CA12JetsCA3FilterjetsPF.src = cms.InputTag("pfNoElectronsCHS")
-process.HEPTopJetsPF.src=cms.InputTag("pfNoElectronsCHS")
-# use all genparticles as src (incl neutrinos etc)
-process.CA12JetsCA3FilterjetsGen.src = cms.InputTag("packedGenParticles")
-
-# create pat jets from reco jets
-# fatjets
+from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
+# add fat CA1.2 pat jet 
 addJetCollection(
     process,
     postfix="",
@@ -78,7 +63,7 @@ addJetCollection(
     labelName = 'CA12PF',
     trackSource = cms.InputTag('unpackedTracksAndVertices'),
     pvSource = cms.InputTag('unpackedTracksAndVertices'),
-    btagDiscriminators = [ 'combinedSecondaryVertexBJetTags' ],
+    btagDiscriminators = [ 'combinedInclusiveSecondaryVertexV2BJetTags','combinedSecondaryVertexBJetTags' ],
     jetCorrections = None,
     getJetMCFlavour = False,
     genJetCollection = None
@@ -92,10 +77,10 @@ addJetCollection(
     labelName = 'CA3SubPF',
     trackSource = cms.InputTag('unpackedTracksAndVertices'),
     pvSource = cms.InputTag('unpackedTracksAndVertices'),
-    btagDiscriminators = [ 'combinedSecondaryVertexBJetTags' ],
+    btagDiscriminators = None,
     jetCorrections = None,
     getJetMCFlavour = False,
-    genJetCollection = cms.InputTag('CA12JetsCA3FilterjetsGen','subjets')
+    genJetCollection = None
 )
 # filterjets (reclustered from subjets)
 addJetCollection(
@@ -106,10 +91,10 @@ addJetCollection(
     labelName = 'CA3FiltPF',
     trackSource = cms.InputTag('unpackedTracksAndVertices'),
     pvSource = cms.InputTag('unpackedTracksAndVertices'),
-    btagDiscriminators = [ 'combinedSecondaryVertexBJetTags' ],
+    btagDiscriminators =None,
     jetCorrections = None,
     getJetMCFlavour = False,
-    genJetCollection = cms.InputTag('CA12JetsCA3FilterjetsGen','filterjets')
+    genJetCollection = None#cms.InputTag('CA12JetsCA3FilterjetsGen','filterjets')
 )
 # fatjet for HEPTopTagger
 addJetCollection(
@@ -120,7 +105,7 @@ addJetCollection(
     labelName = 'HEPTopFatPF',
     trackSource = cms.InputTag('unpackedTracksAndVertices'),
     pvSource = cms.InputTag('unpackedTracksAndVertices'),
-    btagDiscriminators = [ 'combinedSecondaryVertexBJetTags' ],
+    btagDiscriminators =None,
     jetCorrections = None,
     getJetMCFlavour = False,
     genJetCollection = None 
@@ -134,101 +119,46 @@ addJetCollection(
     labelName = 'HEPTopSubPF',
     trackSource = cms.InputTag('unpackedTracksAndVertices'),
     pvSource = cms.InputTag('unpackedTracksAndVertices'),
-    btagDiscriminators = [ 'combinedSecondaryVertexBJetTags' ],
+    btagDiscriminators = [ 'combinedInclusiveSecondaryVertexV2BJetTags','combinedSecondaryVertexBJetTags' ],
     jetCorrections = None,
     getJetMCFlavour = False,
-    genJetCollection = None #cms.InputTag('HEPTopJetsGen', 'subjets')
+    genJetCollection = None 
 )
 
+# match sub, filter and fat jet
+from BoostedTTH.BoostedProducer.SubjetsFilterjetsMatcher_cfi import CA12JetsCA3FilterjetsPFMatcher
+process.CA12JetsCA3FilterjetsPFMatcher=CA12JetsCA3FilterjetsPFMatcher.clone()
+# match top sub to fat jet
+from BoostedTTH.BoostedProducer.HEPTopJetMatcher_cfi import HEPTopJetsPFMatcher
+process.HEPTopJetsPFMatcher=HEPTopJetsPFMatcher.clone()
 
-# load subfilterjet matcher
-process.load("BoostedTTH.BoostedProducer.SubjetsFilterjetsMatcher_cfi")
-
-# load heptopjet matcher
-process.load("BoostedTTH.BoostedProducer.HEPTopJetMatcher_cfi")
-
-
-process.combinedSecondaryVertex.trackMultiplicityMin = 1 #silly sv, uses un filtered tracks.. i.e. any pt
-
-#adjust MC matching
-process.patJetPartons.src = "prunedGenParticles"
-process.patJetPartons.skipFirstN = cms.uint32(0) # do not skip first 6 particles, we already pruned some!
-process.patJetPartons.acceptNoDaughters = cms.bool(True) # as we drop intermediate stuff, we need to accept quarks with no siblings
-process.patJetPartonMatch.matched="prunedGenParticles"
-process.patJetPartons.paticles=cms.InputTag("packedGenParticles")
-process.patJetPartonMatchCA3SubPF.matched="prunedGenParticles"
-process.patJetPartonMatchCA3FiltPF.matched="prunedGenParticles"
-process.patJetPartonMatchCA12PF.matched="prunedGenParticles"
-process.patJetPartonMatchHEPTopFatPF.matched="prunedGenParticles"
-process.patJetPartonMatchHEPTopSubPF.matched="prunedGenParticles"
-
-process.selectedPatJetsCA3FiltPF.cut="pt>10"
-
-process.content = cms.EDAnalyzer("EventContentAnalyzer")
-
-process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('BoostedMiniAOD.root'),
-#    outputCommands = cms.untracked.vstring(['keep *','drop *_pfCHS_*','drop *_selectedMuons_*','drop *_selectedElectrons_*','drop *_pfNoElectronsCHS_*','drop *_pfNoMuonCHS_*','drop *_*CA12*_*_*','drop *_*CA3*_*_*','drop *_*HEP*_*_*','keep *_*Matcher_*_*'])
-)
-
+#adjust MC matching for all jet collections
 process.patJetsCA12PF.addGenJetMatch=False
+process.patJetsCA3SubPF.addGenJetMatch=False
+process.patJetsCA3FiltPF.addGenJetMatch=False
 process.patJetsHEPTopFatPF.addGenJetMatch=False
 process.patJetsHEPTopSubPF.addGenJetMatch=False
+process.patJetPartonMatchCA12PF.matched = "prunedGenParticles"
+process.patJetPartonMatchCA3SubPF.matched = "prunedGenParticles"
+process.patJetPartonMatchCA3FiltPF.matched = "prunedGenParticles"
+process.patJetPartonMatchHEPTopSubPF.matched = "prunedGenParticles"
+process.patJetPartonMatchHEPTopFatPF.matched = "prunedGenParticles"
 
-process.endpath= cms.EndPath(
-    process.pfCHS
-    *process.selectedMuons
-    *process.selectedElectrons
-    *process.pfNoMuonCHS
-    *process.pfNoElectronsCHS
-    *process.unpackedTracksAndVertices
-    *process.filtjet_gen_seq
-    *process.filtjet_pf_seq
-    *process.heptopjet_pf_seq
-    *process.jetTracksAssociatorAtVertexCA3FiltPF
-    *process.jetTracksAssociatorAtVertexCA3SubPF
-    *process.jetTracksAssociatorAtVertexCA12PF
-    *process.jetTracksAssociatorAtVertexHEPTopFatPF
-    *process.jetTracksAssociatorAtVertexHEPTopSubPF
-    *process.impactParameterTagInfosCA3FiltPF
-    *process.impactParameterTagInfosCA3SubPF
-    *process.impactParameterTagInfosCA12PF
-    *process.impactParameterTagInfosHEPTopFatPF
-    *process.impactParameterTagInfosHEPTopSubPF
-    *process.secondaryVertexTagInfosCA3FiltPF
-    *process.secondaryVertexTagInfosCA3SubPF
-    *process.secondaryVertexTagInfosCA12PF
-    *process.secondaryVertexTagInfosHEPTopFatPF
-    *process.secondaryVertexTagInfosHEPTopSubPF
-    *process.combinedSecondaryVertexBJetTagsCA3FiltPF
-    *process.combinedSecondaryVertexBJetTagsCA3SubPF
-    *process.combinedSecondaryVertexBJetTagsCA12PF
-    *process.combinedSecondaryVertexBJetTagsHEPTopFatPF
-    *process.combinedSecondaryVertexBJetTagsHEPTopSubPF
-    *process.patJetChargeCA3FiltPF
-    *process.patJetChargeCA3SubPF
-    *process.patJetChargeCA12PF
-    *process.patJetChargeHEPTopFatPF
-    *process.patJetChargeHEPTopSubPF
-    *process.patJetGenJetMatchCA3FiltPF
-    *process.patJetGenJetMatchCA3SubPF
-    *process.patJetPartonMatchCA3FiltPF
-    *process.patJetPartonMatchCA3SubPF
-    *process.patJetPartonMatchCA12PF
-    *process.patJetPartonMatchHEPTopFatPF
-    *process.patJetPartonMatchHEPTopSubPF
-    *process.patJetsCA3FiltPF
-    *process.patJetsCA3SubPF
-    *process.patJetsCA12PF
-    *process.patJetsHEPTopFatPF
-    *process.patJetsHEPTopSubPF
-    *process.selectedPatJetsCA12PF
-    *process.selectedPatJetsCA3SubPF
-    *process.selectedPatJetsCA3FiltPF
-    *process.selectedPatJetsHEPTopFatPF
-    *process.selectedPatJetsHEPTopSubPF
-    *process.CA12JetsCA3FilterjetsPFMatcher
-    *process.HEPTopJetsPFMatcher
-    *process.out
+#recreate tracks and pv for btagging
+process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
+process.combinedSecondaryVertex.trackMultiplicityMin = 1 #silly sv, uses un filtered tracks.. i.e. any pt (from example, nut sure what this means...)
+
+process.inclusiveVertexFinder.primaryVertices=cms.InputTag("offlineSlimmedPrimaryVertices")
+process.inclusiveVertexFinder.tracks=cms.InputTag("unpackedTracksAndVertices")
+process.trackVertexArbitrator.tracks=cms.InputTag("unpackedTracksAndVertices")
+process.trackVertexArbitrator.primaryVertices=cms.InputTag("offlineSlimmedPrimaryVertices")
+
+
+process.OUT = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string('test.root'),
+    outputCommands = cms.untracked.vstring(['drop *','keep *_*_*_PAT','keep *_*_*_RECO','keep *_*_*_HLT','keep *_*_*_SIM','keep *_*_*_LHE','keep *_*HEPTopJetsPFMatcher_*_*','keep *_*CA12JetsCA3FilterjetsPFMatcher_*_*'])
+
 )
+process.endpath= cms.EndPath(process.OUT)
 
