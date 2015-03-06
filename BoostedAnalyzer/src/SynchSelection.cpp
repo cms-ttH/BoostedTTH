5ea0593abf4d41ba03edd0301f0deae18358d9d2
Karim EL Morabit <uvclu@student.kit.edu>
2015-03-06 17:32:04 +0100

    uncorrected the jets and did Jet cleaning with dR matching

--- BoostedAnalyzer/src/SynchSelection.cpp
+++ BoostedAnalyzer/src/SynchSelection.cpp
@@ -22,6 +22,9 @@ void SynchSelection::Init(const edm::ParameterSet& iConfig, Cutflow& cutflow){
   cutflow.AddStep(">= 1 top tagged jet");
   cutflow.AddStep(">= 1 Higgs jet");
   
+  
+  
+  
   initialized=true;
 }
 
@@ -165,9 +168,7 @@ std::cout<<"doing muons"<<std::endl;
     }
     
   }
-    
-//-------------------------------------------------------------------------
-//do jets
+
 
     
 std::cout<<"doing trigger"<<std::endl;
@@ -201,28 +202,166 @@ vector<string> targetTriggers;
 }
 else hasTriggered=false;
 
+
+
+//----------------------------------------------------------------------------------
+//do jets
+std::vector<pat::Jet> cleanEleJets;
+std::vector<pat::Jet> cleanMuonJets;
+std::vector<pat::Jet> correctedJets;
+std::vector<pat::Jet> sortedJets;
+std::vector<pat::Jet> selectedJets;
+std::vector<pat::Jet> taggedJets;
+
+//first uncorrect the jets
+std::vector<pat::Jet> bufferJets;
+for( std::vector<pat::Jet>::const_iterator it = input.selectedJets.begin(), ed = input.selectedJets.end(); it != ed; ++it ){
+  pat::Jet iJet = *it;
+  double originalPt = iJet.pt();
+  math::XYZTLorentzVector uncorrectedP4 = iJet.correctedP4("Uncorrected");
+  iJet.setP4(uncorrectedP4);
+  double uncorrectedPt = iJet.pt();
+  std::cout<<originalPt<<" "<<uncorrectedPt<<std::endl;
+  bufferJets.push_back(iJet);
+}
+  
+//clean from electrons 
+for(std::vector<pat::Electron>::const_iterator iEle = selectedElectrons.begin(), ed = selectedElectrons.end(); iEle != ed; ++iEle ){
+  pat::Electron Ele = *iEle;
+  double maxDeltaR=0.4;
+  double minDeltaR=maxDeltaR;
+  double dR=999.0;
+  int matchindex=-1;
+  int counter=0;
+  
+  //find jet closes to electron
+  for( std::vector<pat::Jet>::const_iterator it = bufferJets.begin(), ed = bufferJets.end(); it != ed; ++it ){
+      pat::Jet iJet = *it;
+      dR = reco::deltaR(Ele.eta(), Ele.phi(), iJet.eta(), iJet.phi());
+      if(dR<minDeltaR){
+        minDeltaR = dR;
+        matchindex=counter;
+        }
+      counter++;
+    }
+    
+  // now clean the closest jet from the electron and put it back with the rest
+  counter=0;
+  for( std::vector<pat::Jet>::const_iterator it = bufferJets.begin(), ed = bufferJets.end(); it != ed; ++it ){
+      pat::Jet iJet = *it;
+      if(matchindex==counter){
+        math::XYZTLorentzVector original = iJet.p4();
+        original -= Ele.p4();
+        iJet.setP4(original);
+       }
+      if(iJet.pt()>0.0 && iJet.energy()>0.0){
+        cleanEleJets.push_back(iJet);
+       } 
+      counter++;
+   }
+   bufferJets.clear();
+   for( std::vector<pat::Jet>::const_iterator it = cleanEleJets.begin(), ed = cleanEleJets.end(); it != ed; ++it ){
+      bufferJets.push_back(*it);
+   }
+   cleanEleJets.clear();
+}
+for( std::vector<pat::Jet>::const_iterator it = bufferJets.begin(), ed = bufferJets.end(); it != ed; ++it ){
+  cleanEleJets.push_back(*it);
+}
+//now we should have the jets cleaned from the electrons
+//clean from electrons 
+
+bufferJets.clear();
+for( std::vector<pat::Jet>::const_iterator it = cleanEleJets.begin(), ed = cleanEleJets.end(); it != ed; ++it ){
+  bufferJets.push_back(*it);
+}
+
+for(std::vector<pat::Muon>::const_iterator iMuon = selectedMuons.begin(), ed = selectedMuons.end(); iMuon != ed; ++iMuon ){
+  pat::Muon Muon = *iMuon;
+  double maxDeltaR=0.4;
+  double minDeltaR=maxDeltaR;
+  double dR=999.0;
+  int matchindex=-1;
+  int counter=0;
+  
+  //find jet closes to muon
+  for( std::vector<pat::Jet>::const_iterator it = bufferJets.begin(), ed = bufferJets.end(); it != ed; ++it ){
+      pat::Jet iJet = *it;
+      dR = reco::deltaR(Muon.eta(), Muon.phi(), iJet.eta(), iJet.phi());
+      if(dR<minDeltaR){
+        minDeltaR = dR;
+        matchindex=counter;
+        }
+      counter++;
+    }
+    
+  // now clean the closest jet from the muon and put it back with the rest
+  counter=0;
+  for( std::vector<pat::Jet>::const_iterator it = bufferJets.begin(), ed = bufferJets.end(); it != ed; ++it ){
+      pat::Jet iJet = *it;
+      if(matchindex==counter){
+        math::XYZTLorentzVector original = iJet.p4();
+        original -= Muon.p4();
+        iJet.setP4(original);
+       }
+      if(iJet.pt()>0.0 && iJet.energy()>0.0){
+        cleanMuonJets.push_back(iJet);
+       } 
+      counter++;
+   }
+   bufferJets.clear();
+   for( std::vector<pat::Jet>::const_iterator it = cleanMuonJets.begin(), ed = cleanMuonJets.end(); it != ed; ++it ){
+      bufferJets.push_back(*it);
+   }
+   cleanMuonJets.clear();
+}
+for( std::vector<pat::Jet>::const_iterator it = bufferJets.begin(), ed = bufferJets.end(); it != ed; ++it ){
+  cleanMuonJets.push_back(*it);
+  std::cout<<it->pt()<<std::endl;
+}
+
+//now correct the jets again
+//using the PHYS14_25_V2 corrections i hope :)
+/*  "ak4PFchsL1L2L3"*/
+for( std::vector<pat::Jet>::const_iterator it = cleanMuonJets.begin(), ed = cleanMuonJets.end(); it != ed; ++it ){
+  pat::Jet iJet = *it;
+//   double uncorrectedPt = iJet.pt();
+//   math::XYZTLorentzVector correctedP4 = iJet.correctedP4("L1FastJetL2RelativeL3Absolute","","PHYS14_25_V2");
+//   iJet.setP4(correctedP4);
+  pat::Jet newJet = iJet.correctedJet("L3Absolute","none","patJetCorrFactors");
+//   double correctedPt = newJet.pt();
+//   std::cout<<uncorrectedPt<<" "<<correctedPt<<std::endl;
+//   bufferJets.push_back(iJet);
+  
+//   for(unsigned int i=0; i<iJet.availableJECSets().size(); i++){
+//     std::cout<<iJet.availableJECSets()[i]<<std::endl;
+//   }
+//   for(unsigned int i=0; i<iJet.availableJECLevels().size(); i++){
+//     std::cout<<iJet.availableJECLevels("patJetCorrFactors")[i]<<std::endl;
+//   }
+}
     
 //----------------------------------------------------------------------------------
   // now write the numbers that should be dumped
  
-  int run=-999;
-  int lumi=-999;
-  int event=-999;
-  float lep1_pt=-999;
-  float lep1_eta=-999;
-  float lep1_phi=-999;
-  float jet1_pt=-999;
-  float jet2_pt=-999;
-  float jet3_pt=-999; 
-  float jet4_pt=-999;
-  float jet1_CSVv2=-999;
-  float jet2_CSVv2=-999;
-  float jet3_CSVv2=-999;
-  float jet4_CSVv2=-999;
-  int n_jets=-999;
-  int n_btags=-999;
-  int n_toptags=-999; 
-  int n_higgstags=-999;
+  int run=-99;
+  int lumi=-99;
+  int event=-99;
+  float lep1_pt=-99;
+  float lep1_eta=-99;
+  float lep1_phi=-99;
+  float jet1_pt=-99;
+  float jet2_pt=-99;
+  float jet3_pt=-99; 
+  float jet4_pt=-99;
+  float jet1_CSVv2=-99;
+  float jet2_CSVv2=-99;
+  float jet3_CSVv2=-99;
+  float jet4_CSVv2=-99;
+  int n_jets=-99;
+  int n_btags=-99;
+  int n_toptags=-99; 
+  int n_higgstags=-99;
   
   run = int(input.event.run);
   lumi = int(input.event.lumiBlock);
@@ -276,8 +415,12 @@ else hasTriggered=false;
       fclose (pFile);
       } 
     }
+      
+  bool singleLepton=false;
+  if(doElectrons){if(selectedElectrons.size()==1)singleLepton=true;}
+  else{if(selectedMuons.size()==1)singleLepton=true;}
   
-  if(false) return false;
+  if(!singleLepton) return false;
   else{ cutflow.EventSurvivedStep("== 1 lepton");
     if(doElectrons) outfile="KIT_sync_ele_2.txt";
     else outfile="KIT_sync_mu_2.txt";
@@ -296,9 +439,28 @@ else hasTriggered=false;
       } 
     }
     
-  if(false) return false;
-  else cutflow.EventSurvivedStep("== 0 leptons different flavor");
-  
+  bool noOtherLepton=false;
+  if(doElectrons){if(selectedMuons.size()==0)noOtherLepton=true;}
+  else{if(selectedElectrons.size()==1)noOtherLepton=true;}
+  if(!noOtherLepton) return false;
+  else{ cutflow.EventSurvivedStep("== 0 leptons different flavor");
+    if(doElectrons) outfile="KIT_sync_ele_3.txt";
+    else outfile="KIT_sync_mu_3.txt";
+    FILE* pFile;
+    pFile=fopen(outfile.c_str(),"a");
+    if(pFile!=NULL)
+      {
+      fprintf(pFile, "%6d %8d %10d   %6.2f %+4.2f %+4.2f   %6.2f %6.2f %6.2f %6.2f   %+7.3f %+7.3f %+7.3f %+7.3f   %2d  %2d   %2d  %2d\n",
+      run, lumi, event,
+      lep1_pt, lep1_eta, lep1_phi,
+      jet1_pt, jet2_pt, jet3_pt, jet4_pt,
+      jet1_CSVv2, jet2_CSVv2, jet3_CSVv2, jet4_CSVv2,
+      n_jets, n_btags,
+      n_toptags, n_higgstags);
+      fclose (pFile);
+      } 
+    }
+    
   if(false) return false;
   else cutflow.EventSurvivedStep(">= 4 jets");
   
