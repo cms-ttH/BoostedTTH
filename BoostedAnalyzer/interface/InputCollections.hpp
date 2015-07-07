154d20a123983d70112b9c31746b2a4e2d7b46ad
Hannes Mildner <hannes.mildner@cern.ch>
2015-07-07 08:38:49 +0200

    added forward jets to inputcollections

--- BoostedAnalyzer/interface/InputCollections.hpp
+++ BoostedAnalyzer/interface/InputCollections.hpp
@@ -35,6 +35,7 @@ InputCollections(   const EventInfo&                              eventInfo_,
                     const std::vector<pat::Electron>&             selectedElectronsLoose_,
                     const std::vector<pat::Jet>&                  selectedJets_,
                     const std::vector<pat::Jet>&                  selectedJetsLoose_,
+                    const std::vector<pat::Jet>&                  selectedJetsForward_,
                     const pat::MET&                               pfMET_,
                     const boosted::HEPTopJetCollection&           selectedHEPTopJets_,
                     const boosted::SubFilterJetCollection&        selectedSubFilterJets_,
@@ -55,6 +56,7 @@ InputCollections(   const EventInfo&                              eventInfo_,
   selectedElectronsLoose(selectedElectronsLoose_),
   selectedJets(selectedJets_),
   selectedJetsLoose(selectedJetsLoose_),
+  selectedJetsForward(selectedJetsForward_),
   pfMET(pfMET_),
   selectedHEPTopJets(selectedHEPTopJets_),
   selectedSubFilterJets(selectedSubFilterJets_),
@@ -67,6 +69,7 @@ InputCollections(   const EventInfo&                              eventInfo_,
 InputCollections(   const InputCollections&                       input,
                     const std::vector<pat::Jet>&                  selectedJets_,
                     const std::vector<pat::Jet>&                  selectedJetsLoose_,
+                    const std::vector<pat::Jet>&                  selectedJetsForward_,
                     const pat::MET&                               pfMET_,
                     const std::map<std::string,float>&            weights_
 		    ): 
@@ -81,6 +84,7 @@ InputCollections(   const InputCollections&                       input,
   selectedElectronsLoose(input.selectedElectronsLoose),
   selectedJets(selectedJets_),
   selectedJetsLoose(selectedJetsLoose_),
+  selectedJetsForward(selectedJetsForward_),
   pfMET(pfMET_),
   selectedHEPTopJets(input.selectedHEPTopJets),
   selectedSubFilterJets(input.selectedSubFilterJets),
@@ -102,6 +106,7 @@ InputCollections(   const InputCollections&                       input,
   const std::vector<pat::Electron>&             selectedElectronsLoose;
   const std::vector<pat::Jet>&                  selectedJets;
   const std::vector<pat::Jet>&                  selectedJetsLoose;
+  const std::vector<pat::Jet>&                  selectedJetsForward;
   const pat::MET&                               pfMET;
   const boosted::HEPTopJetCollection&           selectedHEPTopJets;
   const boosted::SubFilterJetCollection&        selectedSubFilterJets;
