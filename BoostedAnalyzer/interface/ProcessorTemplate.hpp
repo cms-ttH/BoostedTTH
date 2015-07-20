af86d9ab3ed284262847ed7b15888032a55c7d70
Hannes Mildner <hannes.mildner@cern.ch>
2015-07-20 17:25:36 +0200

    added processor templates

---
+++ BoostedAnalyzer/interface/ProcessorTemplate.hpp
@@ -0,0 +1,21 @@
+#ifndef BOOSTEDTTH_BOOSTEDANALYZER_PROCESSORTEMPLATE_HPP
+#define BOOSTEDTTH_BOOSTEDANALYZER_PROCESSORTEMPLATE_HPP
+
+#include "BoostedTTH/BoostedAnalyzer/interface/TreeProcessor.hpp"
+
+class ProcessorTemplate: public TreeProcessor{
+  
+public:
+  
+  // all configurations should be done in constructor
+  ProcessorTemplate();
+  ~ProcessorTemplate();
+  // you have to implement this to functions, you can add additionional (private) functions
+  void Init(const InputCollections& input,VariableContainer& var);
+  void Process(const InputCollections& input,VariableContainer& var);
+private:
+  // add all members needed, initialize in constructor
+
+};
+
+#endif
---
+++ BoostedAnalyzer/src/ProcessorTemplate.cpp
@@ -0,0 +1,29 @@
+#include "BoostedTTH/BoostedAnalyzer/interface/ProcessorTemplate.hpp"
+
+using namespace std;
+
+// all configurations should be done in constructor
+ProcessorTemplate::ProcessorTemplate(){}
+ProcessorTemplate::~ProcessorTemplate(){}
+
+
+void ProcessorTemplate::Init(const InputCollections& input,VariableContainer& vars){
+  vars.InitVar( "HTjet" ); // initialize a float variable
+  vars.InitVar( "N_Jets","I" ); // initialize an integer variable
+  vars.InitVars( "Jet_Pt","N_Jets" ); // initialize a float-array variable of length N_Jets (notice the *s* in InitVars)
+  
+  initialized=true;
+}
+
+void ProcessorTemplate::Process(const InputCollections& input,VariableContainer& vars){
+  if(!initialized) cerr << "tree processor not initialized" << endl; // complain if not initialized but keep running
+  
+  vars.FillVar( "N_Jets",input.selectedJets.size()); // fill variable N_Jets, type is found automatically
+  float htjet=0.;
+  for(uint i=0; i<input.selectedJets.size() ; i++){
+    vars.FillVars( "Jet_Pt",i,input.selectedJets[i].pt() ); // fill array entry i of Jet_Pt (notice the *s* in FillVars)
+    htjet+=input.selectedJets[i].pt();
+  }     		     
+  vars.FillVar( "HTjet",htjet ); // fill htjets
+
+}
