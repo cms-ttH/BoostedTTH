af7dc9da8f226e35dd1f31e9c4fc08888e67c29f
Hannes Mildner <hannes.mildner@cern.ch>
2015-08-28 17:40:05 +0200

    more dilepton control plots

--- BoostedAnalyzer/src/DiLeptonVarProcessor.cpp
+++ BoostedAnalyzer/src/DiLeptonVarProcessor.cpp
@@ -13,6 +13,21 @@ void DiLeptonVarProcessor::Init(const InputCollections& input,VariableContainer&
   vars.InitVar( "DiMuon_Pt");
   vars.InitVar( "DiMuon_Eta");
   vars.InitVar( "DiMuon_Phi");
+  vars.InitVar( "DiMuon_SS");
+
+  vars.InitVar( "DiElectron_E");
+  vars.InitVar( "DiElectron_M");
+  vars.InitVar( "DiElectron_Pt");
+  vars.InitVar( "DiElectron_Eta");
+  vars.InitVar( "DiElectron_Phi");
+  vars.InitVar( "DiElectron_SS");
+
+  vars.InitVar( "ElMu_E");
+  vars.InitVar( "ElMu_M");
+  vars.InitVar( "ElMu_Pt");
+  vars.InitVar( "ElMu_Eta");
+  vars.InitVar( "ElMu_Phi");
+  vars.InitVar( "ElMu_SS");
 
   initialized=true;
 
@@ -22,22 +37,41 @@ void DiLeptonVarProcessor::Init(const InputCollections& input,VariableContainer&
 void DiLeptonVarProcessor::Process(const InputCollections& input,VariableContainer& vars){
   if(!initialized) cerr << "tree processor not initialized" << endl;
 
-  if(input.selectedMuonsLoose.size()>=2){
-    std::vector<math::XYZTLorentzVector> leptonVecs;
+  if(input.selectedMuonsDL.size()>=2){
     
-    for(std::vector<pat::Muon>::const_iterator itMu = input.selectedMuonsLoose.begin(); itMu != input.selectedMuonsLoose.end(); ++itMu){
-      leptonVecs.push_back(itMu->p4());
-    }
-    std::sort(leptonVecs.begin(), leptonVecs.end(),BoostedUtils::FirstIsHarder);
-    
-    math::XYZTLorentzVector DiMuonVec=leptonVecs.at(0);
-    DiMuonVec+=leptonVecs.at(1);
+    math::XYZTLorentzVector DiMuonVec=input.selectedMuonsDL[0].p4()+input.selectedMuonsDL[1].p4();
 
     vars.FillVar( "DiMuon_E",DiMuonVec.E() );
     vars.FillVar( "DiMuon_M",DiMuonVec.M() );
     vars.FillVar( "DiMuon_Pt",DiMuonVec.Pt() );
     vars.FillVar( "DiMuon_Eta",DiMuonVec.Eta() );
     vars.FillVar( "DiMuon_Phi",DiMuonVec.Phi() );
+    vars.FillVar( "DiMuon_SS",input.selectedMuonsDL[0].charge()*input.selectedMuonsDL[1].charge()>0 );
+  }
+
+  if(input.selectedElectronsDL.size()>=2){
+
+    math::XYZTLorentzVector DiElectronVec=input.selectedElectronsDL[0].p4()+input.selectedElectronsDL[1].p4();
+
+    vars.FillVar( "DiElectron_E",DiElectronVec.E() );
+    vars.FillVar( "DiElectron_M",DiElectronVec.M() );
+    vars.FillVar( "DiElectron_Pt",DiElectronVec.Pt() );
+    vars.FillVar( "DiElectron_Eta",DiElectronVec.Eta() );
+    vars.FillVar( "DiElectron_Phi",DiElectronVec.Phi() );
+
+    vars.FillVar( "DiElectron_SS",input.selectedElectronsDL[0].charge()*input.selectedElectronsDL[1].charge()>0 );
+  }
+
+  if(input.selectedElectronsDL.size()>=1&&input.selectedMuonsDL.size()>=1){
+    
+    math::XYZTLorentzVector ElMuVec=input.selectedElectronsDL[0].p4()+input.selectedMuonsDL[0].p4();
+
+    vars.FillVar( "ElMu_E",ElMuVec.E() );
+    vars.FillVar( "ElMu_M",ElMuVec.M() );
+    vars.FillVar( "ElMu_Pt",ElMuVec.Pt() );
+    vars.FillVar( "ElMu_Eta",ElMuVec.Eta() );
+    vars.FillVar( "ElMu_Phi",ElMuVec.Phi() );
+    vars.FillVar( "ElMu_SS",input.selectedElectronsDL[0].charge()*input.selectedMuonsDL[1].charge()>0 );
   }
 
 
