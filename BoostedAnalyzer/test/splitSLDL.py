25796edf74bc54973b6cd0a5c7a4b6ee9469d16b
Karim <uvclu@student.kit.edu>
2015-09-11 16:29:11 +0200

    added two scripts for synch

---
+++ BoostedAnalyzer/test/splitSLDL.py
@@ -0,0 +1,86 @@
+import sys
+
+filename1 = sys.argv[1]
+
+outfileSL=open("onlySL/SL_"+filename1,"w")
+outfileDL=open("onlyDL/DL_"+filename1,"w")
+
+f1=open(filename1,"r")
+list1=list(f1)
+
+th1=list1[0].replace("\n","").split(",")
+
+listlist=[]
+for l in list1:
+  listlist.append(l.split(","))
+#print listlist
+
+print th1
+
+# find isSL and isDL indices
+isSLidx=3
+isSLidx=4
+for i in range(len(th1)):
+  if th1[i]=="is_SL":
+    isSLidx=i
+  if th1[i]=="is_DL":
+    isDLidx=i
+print isSLidx, isDLidx
+
+outfileSL.write(list1[0])
+outfileDL.write(list1[0])
+
+for i in range(len(list1)):
+  if (listlist[i][isSLidx]=="1" and listlist[i][isDLidx]=="0" ) or ( listlist[i][isSLidx]=="1.0" and listlist[i][isDLidx]=="0.0" ):
+    outfileSL.write(list1[i])
+  if (listlist[i][isSLidx]=="0" and listlist[i][isDLidx]=="1" ) or ( listlist[i][isSLidx]=="0.0" and listlist[i][isDLidx]=="1.0" ):
+    outfileDL.write(list1[i])
+
+outfileSL.close()
+outfileDL.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
