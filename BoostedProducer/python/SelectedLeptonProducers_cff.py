d450b3863257e24c2aade0433350cb976fd57513
Matthias Schroeder <matthias.schroeder@desy.de>
2015-09-15 17:48:35 +0200

    Add producer for selected leptons

---
+++ BoostedProducer/python/SelectedLeptonProducers_cff.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+SelectedElectronProducer = cms.EDProducer(
+    "SelectedLeptonProducer",
+    leptonType = cms.string("electron"),
+
+    era          = cms.string("2015_74x"),
+    analysisType = cms.string("LJ"),
+    sampleID     = cms.int32(9125),
+    isData       = cms.bool(False),
+
+    leptons = cms.InputTag("slimmedElectrons"),
+    ptMin   = cms.double(5),
+    etaMax  = cms.double(2.5),
+    )
+
+
+SelectedMuonProducer = cms.EDProducer(
+    "SelectedLeptonProducer",
+    leptonType = cms.string("muon"),
+
+    era          = cms.string("2015_74x"),
+    analysisType = cms.string("LJ"),
+    sampleID     = cms.int32(9125),
+    isData       = cms.bool(False),
+
+    leptons = cms.InputTag("slimmedMuons"),
+    ptMin   = cms.double(5),
+    etaMax  = cms.double(2.5),
+    )
+
