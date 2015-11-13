ac1ee26aaedf43aede73fab811a11080253a9e92
MarcoA <marco.harrendorf@partner.kit.edu>
2015-11-13 14:35:06 +0100

    Synchronizer updated to output missing events

--- BoostedAnalyzer/src/Synchronizer.cpp
+++ BoostedAnalyzer/src/Synchronizer.cpp
@@ -280,8 +280,8 @@ void Synchronizer::DumpSyncExe2(const InputCollections& input,const InputCollect
   float csv2_fatjet_1=0;
   float csv2_fatjet_2=0;
   
-  const int nEntries = 21;
-  int comparisonList[] = {277215, 324990, 332287, 904458, 1040332, 1059298, 1067268, 1078294, 2111395, 2259390, 2640002, 2844708, 2991300, 3118161, 3808647, 3833513, 3837984, 3838481, 3855514, 3878522, 3896777}; 
+  const int nEntries = 13;
+  int comparisonList[] = {909624, 2222562, 275604,918997,2283465,277215,324990,332287,1059298,1078294,2111395,2259390,2844708 }; 
 
   bool compare = false;
                  
