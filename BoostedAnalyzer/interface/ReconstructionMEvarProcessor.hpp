320d5ea1d2b2502ed89a6595b0c185f071971f34
Hannes Mildner <hannes.mildner@cern.ch>
2015-11-22 12:40:57 +0100

    renamd

--- BoostedAnalyzer/interface/ReconstructionMEvarProcessor.hpp
+++ BoostedAnalyzer/interface/ReconstructionMEvarProcessor.hpp
@@ -1,5 +1,5 @@
-#ifndef BOOSTEDTTH_BOOSTEDANALYZER_RECONSTRUCTIONVARPROCESSOR_HPP
-#define BOOSTEDTTH_BOOSTEDANALYZER_RECONSTRUCTIONVARPROCESSOR_HPP
+#ifndef BOOSTEDTTH_BOOSTEDANALYZER_RECONSTRUCTIONMEVARPROCESSOR_HPP
+#define BOOSTEDTTH_BOOSTEDANALYZER_RECONSTRUCTIONMEVARPROCESSOR_HPP
 
 #include "BoostedTTH/BoostedAnalyzer/interface/TreeProcessor.hpp"
 #include "BoostedTTH/TTHReconstruction/interface/ReconstructionQuality.hpp"
@@ -8,13 +8,13 @@
 #include "BoostedTTH/TTHReconstruction/interface/ReconstructionMCMatching.hpp"
 #include "TLorentzVector.h"
 
-class ReconstructionVarProcessor: public TreeProcessor{
+class ReconstructionMEvarProcessor: public TreeProcessor{
 
 public:
 
-    ReconstructionVarProcessor();
+    ReconstructionMEvarProcessor();
 
-    ~ReconstructionVarProcessor();
+    ~ReconstructionMEvarProcessor();
     
     void Init(const InputCollections& input,VariableContainer& var);
     
