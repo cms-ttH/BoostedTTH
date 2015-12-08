a34f0e559238475d8c1d188237a58e9aeea506ee
Hannes Mildner <hannes.mildner@cern.ch>
2015-12-08 10:36:40 +0100

    added common BDT

--- BoostedAnalyzer/src/BDTVarProcessor.cpp
+++ BoostedAnalyzer/src/BDTVarProcessor.cpp
@@ -13,6 +13,7 @@ void BDTVarProcessor::Init(const InputCollections& input,VariableContainer& vars
   vars.InitVar("BDTOhio_v2_output");
   vars.InitVar("BDT_v3_output");
   vars.InitVar("BDT_v4_output");
+  vars.InitVar("BDT_common");
 
   map<string,float> bdtinputs2=bdtohio2.GetVariablesOfLastEvaluation();
   for(auto it=bdtinputs2.begin(); it!=bdtinputs2.end(); it++){
@@ -27,6 +28,11 @@ void BDTVarProcessor::Init(const InputCollections& input,VariableContainer& vars
   map<string,float> bdtinputs4=bdt4.GetVariablesOfLastEvaluation();
   for(auto it=bdtinputs4.begin(); it!=bdtinputs4.end(); it++){
     vars.InitVar("BDT_v4_input_"+it->first);
+  }
+
+  map<string,float> bdtinputs_common=bdt4.GetVariablesOfLastEvaluation();
+  for(auto it=bdtinputs_common.begin(); it!=bdtinputs_common.end(); it++){
+    vars.InitVar("BDT_common_"+it->first);
   }
 
   initialized=true;
@@ -60,6 +66,28 @@ void BDTVarProcessor::Process(const InputCollections& input,VariableContainer& v
     for(auto it=bdtinputs4.begin(); it!=bdtinputs4.end(); it++){
       vars.FillVar("BDT_v4_input_"+it->first,it->second);
     }
+  }
+  
+  vector<TLorentzVector> lepvecs=BoostedUtils::GetTLorentzVectors(BoostedUtils::GetLepVecs(input.selectedElectrons,input.selectedMuons));
+  vector<TLorentzVector> jetvecs=BoostedUtils::GetTLorentzVectors(BoostedUtils::GetJetVecs(input.selectedJets));
+  vector<TLorentzVector> loose_jetvecs=BoostedUtils::GetTLorentzVectors(BoostedUtils::GetJetVecs(input.selectedJetsLoose));
+  TLorentzVector metP4=BoostedUtils::GetTLorentzVector(input.pfMET.p4());
+  vector<double> jetcsvs;
+  vector<double> loose_jetcsvs;
+  for(auto j=input.selectedJets.begin(); j!=input.selectedJets.end(); j++){
+      jetcsvs.push_back(BoostedUtils::GetJetCSV(*j));
+  }
+  for(auto j=input.selectedJetsLoose.begin(); j!=input.selectedJetsLoose.end(); j++){
+      loose_jetcsvs.push_back(BoostedUtils::GetJetCSV(*j));
+  }
+
+  float bdtoutput_common=commonBDT.GetBDTOutput(lepvecs, jetvecs, jetcsvs,loose_jetvecs,loose_jetcsvs,metP4);
+  vars.FillVar("BDT_v_common_output",bdtoutput_common);
+  if(commonBDT.GetCategoryOfLastEvaluation()!="none"){
+      map<string,float> bdtinputs_common=commonBDT.GetVariablesOfLastEvaluation();
+      for(auto it=bdtinputs_common.begin(); it!=bdtinputs_common.end(); it++){
+	  vars.FillVar("BDT_v_common_input_"+it->first,it->second);
+      }
   }
 
 
