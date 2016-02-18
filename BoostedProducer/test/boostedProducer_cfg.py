f2766aaca34d466723baf784b1dc284b85318d37
Shawn Williamson <shawn.williamson@kit.edu>
2016-02-18 17:44:35 +0100

    BoostedProducer Config for Updated SelectedLeptonProducer

--- BoostedProducer/test/boostedProducer_cfg.py
+++ BoostedProducer/test/boostedProducer_cfg.py
@@ -5,7 +5,7 @@ process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring('file:/pnfs/desy.de/cms/tier2/store/mc/RunIIFall15MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext3-v1/00000/0634456A-08C2-E511-A0C1-001E6739722E.root')
                             #fileNames = cms.untracked.vstring('root://cmsxrootd.fnal.gov///store/mc/RunIIFall15MiniAODv2/TT_TuneEE5C_13TeV-amcatnlo-herwigpp/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/10000/00129514-9FB8-E511-9C7A-00266CFFC544.root')
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 # messages
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -32,7 +32,7 @@ process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+process.GlobalTag = GlobalTag(process.GlobalTag, '76X_mcRun2_asymptotic_RunIIFall15DR76_v0')
 
 # make Boosted Jets
 process.load('BoostedTTH.BoostedProducer.BoostedJetProducer_cfi')
@@ -157,8 +157,6 @@ process.load("BoostedTTH.BoostedProducer.genHadronMatching_cfi")
 # skim
 process.load("BoostedTTH.BoostedProducer.LeptonJetsSkim_cfi")
 
-process.content = cms.EDAnalyzer("EventContentAnalyzer")
-
 # execute in the right order
 process.boosted_skimmed=cms.Path(process.electronMVAValueMapProducer
                                  *process.LeptonJetsSkim
@@ -171,9 +169,7 @@ process.boosted_skimmed=cms.Path(process.electronMVAValueMapProducer
                                  *process.patJetsSFFatJetsPF
                                  *process.patJetsSFSubjetsPF
                                  *process.patJetsSFFilterjetsPF
-                                 *process.BoostedJetMatcher
-                                 )
-
+                                 *process.BoostedJetMatcher)
 
 process.OUT = cms.OutputModule(
     "PoolOutputModule",
