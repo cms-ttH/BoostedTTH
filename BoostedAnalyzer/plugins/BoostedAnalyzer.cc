a41c69da43155393907bf3bc7c96f24012bdcf15
Michael Wassmer <mwassmer@nafhh-cms06.desy.de>
2016-06-21 15:07:32 +0200

    added pdf weights and q2 weights

--- BoostedAnalyzer/plugins/BoostedAnalyzer.cc
+++ BoostedAnalyzer/plugins/BoostedAnalyzer.cc
@@ -698,7 +698,7 @@ void BoostedAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	}
 	if(selected) treewriters[i_sys]->Process(inputs[i_sys]);
     }
-
+   
 }
 
 float BoostedAnalyzer::GetTopPtWeight(float toppt1,float toppt2){
