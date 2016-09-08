ee19c3ecf5a026a28efc75dcac52ee1eb38d86a0
Korbinian Schweiger <korbinian.schweiger@student.kit.edu>
2016-09-08 14:08:37 +0200

    Bugfix for "Evt_M_MinDeltaRLeptonTaggedJet"

--- BoostedAnalyzer/src/MVAVarProcessor.cpp
+++ BoostedAnalyzer/src/MVAVarProcessor.cpp
@@ -299,7 +299,7 @@ void MVAVarProcessor::Process(const InputCollections& input,VariableContainer& v
   if(selectedTaggedJets.size()>1&&(input.selectedElectrons.size()>0||input.selectedMuons.size()>0)){
     int idClosestTaggedJet = -1;
     float minDrLepTaggedJet = BoostedUtils::GetClosestLepJetID(idClosestTaggedJet,primLepVec,selectedTaggedJets);
-    math::XYZTLorentzVector closestTaggedJetVec = input.selectedJets[idClosestTaggedJet].p4();
+    math::XYZTLorentzVector closestTaggedJetVec = selectedTaggedJets[idClosestTaggedJet].p4();
     vars.FillVar("Evt_M_MinDeltaRLeptonTaggedJet",(primLepVec+closestTaggedJetVec).M());
     vars.FillVar("Evt_Dr_MinDeltaRLeptonTaggedJet",minDrLepTaggedJet);
   }
