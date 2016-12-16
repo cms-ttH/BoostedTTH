a12dc1b54e1bcdd3e824415f4a422169b2b07b00
Felix Riese <mail@felixriese.de>
2016-12-16 13:06:22 +0100

    Sort ak4 jets of ak4cluster by pt

--- BoostedAnalyzer/src/BoostedAk4VarProcessor.cpp
+++ BoostedAnalyzer/src/BoostedAk4VarProcessor.cpp
@@ -8,15 +8,46 @@ BoostedAk4VarProcessor::~BoostedAk4VarProcessor(){}
 
 void BoostedAk4VarProcessor::Init(const InputCollections& input,VariableContainer& vars){
 
-  vars.InitVars( "BoostedAk4Cluster_E","N_BoostedAk4Cluster" );
-  vars.InitVars( "BoostedAk4Cluster_M","N_BoostedAk4Cluster" );
-  vars.InitVars( "BoostedAk4Cluster_Pt","N_BoostedAk4Cluster" );
-  vars.InitVars( "BoostedAk4Cluster_Phi","N_BoostedAk4Cluster" );
-  vars.InitVars( "BoostedAk4Cluster_Eta","N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_E",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_M",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Pt",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Phi",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Eta",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_N_Ak4Jets",-9,"N_BoostedAk4Cluster" );
 
-  vars.InitVars( "BoostedAk4Cluster_Dr_Ak4Cluster_BoostedJet1","N_BoostedAk4Cluster" );
-  vars.InitVars( "BoostedAk4Cluster_Dr_Ak4Cluster_BoostedJet2","N_BoostedAk4Cluster" );
-  vars.InitVars( "BoostedAk4Cluster_Dr_Ak4Cluster_BoostedJet3","N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Dr_Ak4Cluster_BoostedJet1",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Dr_Ak4Cluster_BoostedJet2",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Dr_Ak4Cluster_BoostedJet3",-9,"N_BoostedAk4Cluster" );
+  
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet1_E",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet1_M",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet1_Pt",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet1_Phi",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet1_Eta",-9,"N_BoostedAk4Cluster" );
+  
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet2_E",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet2_M",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet2_Pt",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet2_Phi",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet2_Eta",-9,"N_BoostedAk4Cluster" );
+  
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet3_E",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet3_M",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet3_Pt",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet3_Phi",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet3_Eta",-9,"N_BoostedAk4Cluster" );
+  
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet4_E",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet4_M",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet4_Pt",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet4_Phi",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet4_Eta",-9,"N_BoostedAk4Cluster" );
+  
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet5_E",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet5_M",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet5_Pt",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet5_Phi",-9,"N_BoostedAk4Cluster" );
+  vars.InitVars( "BoostedAk4Cluster_Ak4Jet5_Eta",-9,"N_BoostedAk4Cluster" );
 
   initialized=true;
 }
@@ -30,7 +61,17 @@ void BoostedAk4VarProcessor::Process(const InputCollections& input,VariableConta
     vars.FillVars( "BoostedAk4Cluster_M",i,input.selectedAk4Cluster[i].fatjet.M() );
     vars.FillVars( "BoostedAk4Cluster_Eta",i,input.selectedAk4Cluster[i].fatjet.Eta() );
     vars.FillVars( "BoostedAk4Cluster_Phi",i,input.selectedAk4Cluster[i].fatjet.Phi() );
-
+    vars.FillVars( "BoostedAk4Cluster_N_Ak4Jets",i,input.selectedAk4Cluster[i].ak4jets.size() );
+    
+    for(std::vector<pat::Jet>::const_iterator itAk4Jet = input.selectedAk4Cluster[i].ak4jets.begin(); itAk4Jet != input.selectedAk4Cluster[i].ak4jets.end() && itAk4Jet-input.selectedAk4Cluster[i].ak4jets.begin()<5; ++itAk4Jet){
+      string iAk4JetNumber = std::to_string(itAk4Jet-input.selectedAk4Cluster[i].ak4jets.begin()+1);
+      vars.FillVars( "BoostedAk4Cluster_Ak4Jet"+iAk4JetNumber+"_E",i,itAk4Jet->energy());
+      vars.FillVars( "BoostedAk4Cluster_Ak4Jet"+iAk4JetNumber+"_M",i,itAk4Jet->mass());
+      vars.FillVars( "BoostedAk4Cluster_Ak4Jet"+iAk4JetNumber+"_Pt",i,itAk4Jet->pt());
+      vars.FillVars( "BoostedAk4Cluster_Ak4Jet"+iAk4JetNumber+"_Phi",i,itAk4Jet->phi());
+      vars.FillVars( "BoostedAk4Cluster_Ak4Jet"+iAk4JetNumber+"_Eta",i,itAk4Jet->eta());
+    }
+    
     if(input.selectedBoostedJets.size()>0)
       vars.FillVars( "BoostedAk4Cluster_Dr_Ak4Cluster_BoostedJet1",i, BoostedUtils::DeltaR(input.selectedAk4Cluster[i].fatjet, input.selectedBoostedJets[0].fatjet.p4()));
     if(input.selectedBoostedJets.size()>1)
