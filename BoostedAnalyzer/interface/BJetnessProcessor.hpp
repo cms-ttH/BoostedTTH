fd49df5c092b18ce2d7d84e39ffb593c6f5573a5
Hannes Mildner <hannes.mildner@cern.ch>
2017-01-10 16:41:06 +0100

    removed old bjetness code

--- BoostedAnalyzer/interface/BJetnessProcessor.hpp
+++ BoostedAnalyzer/interface/BJetnessProcessor.hpp
@@ -2,7 +2,6 @@
 #define BOOSTEDTTH_BOOSTEDANALYZER_BJETNESSSPROCESSOR_HPP
 
 #include "BoostedTTH/BoostedAnalyzer/interface/TreeProcessor.hpp"
-#include "Other/BJetness/interface/BJetness.hpp"
 
 
 class BJetnessProcessor: public TreeProcessor{
@@ -17,17 +16,6 @@ public:
   void Process(const InputCollections& input,VariableContainer& var);
 private:
   // add all members needed, initialize in constructor
-  BJetness bjetness;
-  edm::EDGetTokenT< edm::View<pat::Electron> > EDMElectronsToken;
-  edm::EDGetTokenT< edm::View<pat::Muon> > EDMMuonsToken;
-  template <typename T> T GetSortedByCSV(const T&);
 
 };
-template <typename T> T BJetnessProcessor::GetSortedByCSV(const T& collection){
-  T result = collection;
-  std::sort(result.begin(), result.end(), [] (typename T::value_type a, typename T::value_type b) { return
-	      a.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags")>b.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags");});
-  return result;
-}
-
 #endif
--- BoostedAnalyzer/src/BJetnessProcessor.cpp
+++ BoostedAnalyzer/src/BJetnessProcessor.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 // all configurations should be done in constructor
 BJetnessProcessor::BJetnessProcessor(edm::ConsumesCollector && iC){
-    EDMMuonsToken           = iC.consumes< edm::View<pat::Muon> >(edm::InputTag("slimmedMuons","",""));
-    EDMElectronsToken       = iC.consumes< edm::View<pat::Electron> >(edm::InputTag("slimmedElectrons","",""));
-
 }
 BJetnessProcessor::~BJetnessProcessor(){}
 
@@ -25,13 +22,6 @@ void BJetnessProcessor::Init(const InputCollections& input,VariableContainer& va
 
 void BJetnessProcessor::Process(const InputCollections& input,VariableContainer& vars){
     if(!initialized) cerr << "tree processor not initialized" << endl; // complain if not initialized but keep running
-    edm::ESHandle<TransientTrackBuilder> ttrkbuilder;
-    input.iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder",ttrkbuilder);
-    edm::Handle< edm::View<pat::Electron> > h_electrons;
-    input.iEvent.getByToken( EDMElectronsToken,h_electrons );
-    edm::Handle< edm::View<pat::Muon> > h_muons;
-    input.iEvent.getByToken( EDMMuonsToken,h_muons );
-
 
     double num_loosenoipnoiso_leps=-999;
 //     double numjettrksnopv=-999;
@@ -41,19 +31,6 @@ void BJetnessProcessor::Process(const InputCollections& input,VariableContainer&
     double avsip3d_sig=-999;
     double avip1d_sig=-999;
     
-//     std::cout<<input.selectedJets.size()<<std::endl;
-    vector<pat::Jet> jets_by_csv=GetSortedByCSV(input.selectedJets);
-    vector<pat::Jet> bjetness_jets;
-    const uint maxjets=6;
-    uint realMaxJets=maxjets;
-    if (jets_by_csv.size()<maxjets){realMaxJets=jets_by_csv.size();}
-    for(uint i=1; i<realMaxJets;  i++){
-	bjetness_jets.push_back(jets_by_csv[i]);
-    }
-    bjetness.get_bjetness_vars(bjetness_jets,input.selectedPVs[0],*ttrkbuilder,h_electrons,h_muons,
-			       num_loosenoipnoiso_leps, 
-			       pvTrkOVcollTrk, avip3d_val, avip3d_sig, 
-			       avsip3d_sig, avip1d_sig);
 
     vars.FillVar("BJN_N_Leptons",num_loosenoipnoiso_leps);
 //     vars.FillVar("BJN_N_TracksNoPV",numjettrksnopv);
