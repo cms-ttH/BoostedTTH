6d12f9ebd056204b6a12ddbc1dc1d83b2d745bd1
Michael Wassmer <michael.wassmer@cern.ch>
2017-12-01 14:40:10 +0100

    more info in GenDarkMatterEvent

--- BoostedAnalyzer/interface/GenDarkMatterEvent.hpp
+++ BoostedAnalyzer/interface/GenDarkMatterEvent.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
+#include <TLorentzVector.h>
 
 class GenDarkMatterEvent{
   
@@ -12,16 +13,32 @@ class GenDarkMatterEvent{
         // destructor
         ~GenDarkMatterEvent();
         void Initialize(std::vector<reco::GenParticle> prunedGenParticles_, std::vector<pat::PackedGenParticle> packedGenParticles_);
+        void Fill();
+        
         std::vector<reco::GenParticle> ReturnPrunedGenParticles();
         std::vector<pat::PackedGenParticle> ReturnPackedGenParticles();
-        void Fill();
+        
+        reco::GenParticle ReturnMediator();
+        std::vector<reco::GenParticle> ReturnNeutralinos();
+        
+        bool HasDarkMatter();
+        bool IsFilled();
+        
+        double ReturnMediatorMass();
+        TLorentzVector ReturnMediator4Vector();
+        std::vector<double> ReturnNeutralinoMasses();
+        std::vector<TLorentzVector> ReturnNeutralino4Vectors();
+        
+        double ReturnNaiveMET();
   
     private:
         bool hasDarkMatter = false;
+        bool isFilled = false;
         std::vector<reco::GenParticle> prunedGenParticles;
         std::vector<pat::PackedGenParticle> packedGenParticles;
         std::vector<reco::GenParticle> Neutralinos;
         reco::GenParticle Mediator;
+        std::vector<reco::GenParticle> Neutrinos;
 };
 
 
--- BoostedAnalyzer/src/GenDarkMatterEvent.cpp
+++ BoostedAnalyzer/src/GenDarkMatterEvent.cpp
@@ -1,9 +1,10 @@
 #include "BoostedTTH/BoostedAnalyzer/interface/GenDarkMatterEvent.hpp"
+#include "BoostedTTH/BoostedAnalyzer/interface/BoostedUtils.hpp"
 
 // constructor
 GenDarkMatterEvent::GenDarkMatterEvent() {}
 
-// desturctor
+// destructor
 GenDarkMatterEvent::~GenDarkMatterEvent() {}
 
 // take the given genparticle collections and save a copy of them as member variables
@@ -13,6 +14,7 @@ void GenDarkMatterEvent::Initialize(std::vector<reco::GenParticle> prunedGenPart
     packedGenParticles = packedGenParticles_;
     
     hasDarkMatter = false;
+    isFilled = false;
     
     for(size_t i=0;i<prunedGenParticles.size();i++){
         if(prunedGenParticles[i].pdgId()==1000022){
@@ -46,11 +48,82 @@ void GenDarkMatterEvent::Fill()
     // find the lightest neutralinos in the event and the mediator
     for(size_t i=0;i<prunedGenParticles.size();i++){
         reco::GenParticle genparticle = prunedGenParticles[i];
-        if(genparticle.pdgId()==1000022 and genparticle.isLastCopy()){
+        if(genparticle.pdgId()==1000022 and genparticle.isLastCopy() and genparticle.status()==1){
             Neutralinos.push_back(genparticle);
         }
         if(genparticle.pdgId()==23 and genparticle.isLastCopy()) {
             Mediator = genparticle;
         }
+        if((abs(genparticle.pdgId())==12 or abs(genparticle.pdgId())==14 or abs(genparticle.pdgId())==16) and genparticle.isLastCopy() and genparticle.status()==1){
+            Neutrinos.push_back(genparticle);
+        }
     }
+    isFilled = true;
+}
+
+std::vector<reco::GenParticle> GenDarkMatterEvent::ReturnNeutralinos()
+{
+    if(not isFilled){
+        std::cerr << "Attention: Neutralinos are not filled!";
+    }
+    return Neutralinos;
+}
+
+reco::GenParticle GenDarkMatterEvent::ReturnMediator()
+{
+    if(not isFilled){
+        std::cerr << "Attention: Mediator is not filled!";
+    }
+    return Mediator;
+}
+
+bool GenDarkMatterEvent::IsFilled()
+{
+    return isFilled;
+}
+
+bool GenDarkMatterEvent::HasDarkMatter()
+{
+    return hasDarkMatter;
+}
+
+double GenDarkMatterEvent::ReturnMediatorMass()
+{
+    return Mediator.mass();
+}
+
+std::vector<double> GenDarkMatterEvent::ReturnNeutralinoMasses()
+{
+    std::vector<double> masses;
+    for(size_t i=0;i<Neutralinos.size();i++){
+        masses.push_back(Neutralinos[i].mass());
+    }
+    return masses;
+}
+
+TLorentzVector GenDarkMatterEvent::ReturnMediator4Vector()
+{
+    math::XYZTLorentzVector vec4 = Mediator.p4();
+    return BoostedUtils::GetTLorentzVector(vec4);
+}
+
+std::vector<TLorentzVector> GenDarkMatterEvent::ReturnNeutralino4Vectors()
+{
+    std::vector<math::XYZTLorentzVector> vecs4;
+    for(size_t i=0;i<Neutralinos.size();i++){
+        vecs4.push_back(Neutralinos[i].p4());
+    }
+    return BoostedUtils::GetTLorentzVectors(vecs4);
+}
+
+double GenDarkMatterEvent::ReturnNaiveMET()
+{
+    math::XYZTLorentzVector vec4_invisibles;
+    for(size_t i=0;i<Neutralinos.size();i++){
+        vec4_invisibles+=Neutralinos[i].p4();
+    }
+    for(size_t i=0;i<Neutrinos.size();i++){
+        vec4_invisibles+=Neutrinos[i].p4();
+    }
+    return vec4_invisibles.pt();
 }
