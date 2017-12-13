ecf78b9ce3e67f02b7605bc889e505869bf284fb
Michael Wassmer <michael.wassmer@cern.ch>
2017-12-13 14:43:34 +0100

    added more variables to DarkMatterProcessor

--- BoostedAnalyzer/src/DarkMatterProcessor.cpp
+++ BoostedAnalyzer/src/DarkMatterProcessor.cpp
@@ -19,6 +19,10 @@ void DarkMatterProcessor::Init(const InputCollections& input,VariableContainer& 
   vars.InitVar( "Mediator_Pt" );
   vars.InitVar( "Mediator_Phi" );
   vars.InitVar( "Mediator_Eta" );
+  vars.InitVar( "Mediator_Energy" );
+  vars.InitVar( "Mediator_Px" );
+  vars.InitVar( "Mediator_Py" );
+  vars.InitVar( "Mediator_Pz" );
   
   vars.InitVar( "Neutralino1_Mass" );
   vars.InitVar( "Neutralino2_Mass" );
@@ -28,6 +32,35 @@ void DarkMatterProcessor::Init(const InputCollections& input,VariableContainer& 
   vars.InitVar( "Neutralino2_Phi" );
   vars.InitVar( "Neutralino1_Eta" );
   vars.InitVar( "Neutralino2_Eta" );
+  vars.InitVar( "Neutralino1_Energy" );
+  vars.InitVar( "Neutralino2_Energy" );
+  vars.InitVar( "Neutralino1_Px" );
+  vars.InitVar( "Neutralino2_Px" );
+  vars.InitVar( "Neutralino1_Py" );
+  vars.InitVar( "Neutralino2_Py" );
+  vars.InitVar( "Neutralino1_Pz" );
+  vars.InitVar( "Neutralino2_Pz" );
+  
+  vars.InitVar( "N1_N2_Mass" );
+  vars.InitVar( "N1_N2_Pt" );
+  vars.InitVar( "N1_N2_Phi" );
+  vars.InitVar( "N1_N2_Eta" );
+  vars.InitVar( "N1_N2_Energy" );
+  vars.InitVar( "N1_N2_Px" );
+  vars.InitVar( "N1_N2_Py" );
+  vars.InitVar( "N1_N2_Pz" );
+  
+  vars.InitVar( "cos_theta_N1_N2" );
+  vars.InitVar( "cos_theta_Med_N1" );
+  vars.InitVar( "cos_theta_Med_N2" );
+  
+  vars.InitVar( "delta_phi_N1_N2" );
+  vars.InitVar( "delta_phi_Med_N1" );
+  vars.InitVar( "delta_phi_Med_N2" );
+  
+  vars.InitVar( "delta_R_N1_N2" );
+  vars.InitVar( "delta_R_Med_N1" );
+  vars.InitVar( "delta_R_Med_N2" );
 
   initialized=true;
 }
@@ -55,6 +88,10 @@ void DarkMatterProcessor::Process(const InputCollections& input,VariableContaine
   vars.FillVar( "Mediator_Pt",Mediator_p4.Pt());
   vars.FillVar( "Mediator_Phi",Mediator_p4.Phi());
   vars.FillVar( "Mediator_Eta",Mediator_p4.Eta());
+  vars.FillVar( "Mediator_Energy",Mediator_p4.E());
+  vars.FillVar( "Mediator_Px",Mediator_p4.Px());
+  vars.FillVar( "Mediator_Py",Mediator_p4.Py());
+  vars.FillVar( "Mediator_Pz",Mediator_p4.Pz());
   
   vars.FillVar( "Neutralino1_Mass",Neutralinos_p4[0].M());
   vars.FillVar( "Neutralino2_Mass",Neutralinos_p4[1].M());
@@ -64,6 +101,35 @@ void DarkMatterProcessor::Process(const InputCollections& input,VariableContaine
   vars.FillVar( "Neutralino2_Phi",Neutralinos_p4[1].Phi());
   vars.FillVar( "Neutralino1_Eta",Neutralinos_p4[0].Eta());
   vars.FillVar( "Neutralino2_Eta",Neutralinos_p4[1].Eta());
+  vars.FillVar( "Neutralino1_Energy",Neutralinos_p4[0].E());
+  vars.FillVar( "Neutralino2_Energy",Neutralinos_p4[1].E());
+  vars.FillVar( "Neutralino1_Px",Neutralinos_p4[0].Px());
+  vars.FillVar( "Neutralino2_Px",Neutralinos_p4[1].Px());
+  vars.FillVar( "Neutralino1_Py",Neutralinos_p4[0].Py());
+  vars.FillVar( "Neutralino2_Py",Neutralinos_p4[1].Py());
+  vars.FillVar( "Neutralino1_Pz",Neutralinos_p4[0].Pz());
+  vars.FillVar( "Neutralino2_Pz",Neutralinos_p4[1].Pz());
+  
+  vars.FillVar( "N1_N2_Mass",(Neutralinos_p4[0]+Neutralinos_p4[1]).M());
+  vars.FillVar( "N1_N2_Pt",(Neutralinos_p4[0]+Neutralinos_p4[1]).Pt());
+  vars.FillVar( "N1_N2_Phi",(Neutralinos_p4[0]+Neutralinos_p4[1]).Phi());
+  vars.FillVar( "N1_N2_Eta",(Neutralinos_p4[0]+Neutralinos_p4[1]).Eta());
+  vars.FillVar( "N1_N2_Energy",(Neutralinos_p4[0]+Neutralinos_p4[1]).E());
+  vars.FillVar( "N1_N2_Px",(Neutralinos_p4[0]+Neutralinos_p4[1]).Px());
+  vars.FillVar( "N1_N2_Py",(Neutralinos_p4[0]+Neutralinos_p4[1]).Py());
+  vars.FillVar( "N1_N2_Pz",(Neutralinos_p4[0]+Neutralinos_p4[1]).Pz());
+  
+  vars.FillVar( "cos_theta_N1_N2",TMath::Cos((Neutralinos_p4[0].Vect()).Angle(Neutralinos_p4[1].Vect())));
+  vars.FillVar( "cos_theta_Med_N1",TMath::Cos((Neutralinos_p4[0].Vect()).Angle(Mediator_p4.Vect())));
+  vars.FillVar( "cos_theta_Med_N2",TMath::Cos((Neutralinos_p4[1].Vect()).Angle(Mediator_p4.Vect())));
+  
+  vars.FillVar( "delta_phi_N1_N2",Neutralinos_p4[0].DeltaPhi(Neutralinos_p4[1]));
+  vars.FillVar( "delta_phi_Med_N1",Mediator_p4.DeltaPhi(Neutralinos_p4[0]));
+  vars.FillVar( "delta_phi_Med_N2",Mediator_p4.DeltaPhi(Neutralinos_p4[1]));
+  
+  vars.FillVar( "delta_R_N1_N2",Neutralinos_p4[0].DeltaR(Neutralinos_p4[1]));
+  vars.FillVar( "delta_R_Med_N1",Mediator_p4.DeltaR(Neutralinos_p4[0]));
+  vars.FillVar( "delta_R_Med_N2",Mediator_p4.DeltaR(Neutralinos_p4[1]));
   
   }
 }
