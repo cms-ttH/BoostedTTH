48c6e2734a483fa922302dfb637ceaf881fa30c7
Michael Wassmer <michael.wassmer@cern.ch>
2018-02-12 11:54:25 +0100

    prepared crab configs for skimming of MCs for summer18

--- BoostedAnalyzer/test/skim.py
+++ BoostedAnalyzer/test/skim.py
@@ -1,14 +1,28 @@
 import FWCore.ParameterSet.Config as cms
-# input
-isData=False
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('analysis')
+options.register( "isData", False, VarParsing.multiplicity.singleton, VarParsing.varType.bool, "is it data or MC?" )
+options.register( "skipEvents", 0, VarParsing.multiplicity.singleton, VarParsing.varType.int, "Number of events to skip" )
+
+options.parseArguments()
+
+if options.maxEvents is -1: # maxEvents is set in VarParsing class by default to -1
+    options.maxEvents = 10000 # reset for testing
+
+if not options.inputFiles:
+    options.inputFiles=['root://xrootd-cms.infn.it//store/mc/RunIIFall17MiniAOD/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/00000/0081D8CE-41F2-E711-A8FD-FA163E3BE999.root']
 
 process = cms.Process("p")
+#set some defaults
+
+
 process.source = cms.Source("PoolSource",
-#                            fileNames = cms.untracked.vstring('file:/pnfs/desy.de/cms/tier2/store/mc/RunIISummer16MiniAODv2/TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/0030B9D6-72C1-E611-AE49-02163E00E602.root')
-                            fileNames = cms.untracked.vstring('file:/pnfs/desy.de/cms/tier2/store/data/Run2016D/SingleMuon/MINIAOD/23Sep2016-v1/010000/0CEAC70F-C79B-E611-BE1E-00266CFFC7CC.root')
+                            fileNames = cms.untracked.vstring(options.inputFiles),
+                            skipEvents=cms.untracked.uint32(int(options.skipEvents))
 
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
 
 # messages
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -40,20 +54,21 @@ process.ak4PFchsL1L2L3 = cms.ESProducer("JetCorrectionESChain",
     'ak4PFchsL2Relative',
     'ak4PFchsL3Absolute')
 )
-if isData:
+if options.isData==True:
   process.ak4PFchsL1L2L3.correctors.append('ak4PFchsResidual') # add residual JEC for data
 
 
 process.load("BoostedTTH.BoostedAnalyzer.LeptonJetsSkim_cfi")
+process.LeptonJetsSkim.isData=cms.bool(options.isData)
 
-process.boosted_skimmed=cms.Path(process.LeptonJetsSkim)
+process.skimmed=cms.Path(process.LeptonJetsSkim)
 
 process.OUT = cms.OutputModule(
     "PoolOutputModule",
     fileName = cms.untracked.string('Skim.root'),
     outputCommands = cms.untracked.vstring(['drop *','keep *_*_*_PAT','keep *_*_*_RECO','keep *_*_*_HLT*','keep *_*_*_SIM','keep *_*_*_LHE','keep *_matchGen*Hadron_*_*', 'keep *_ak4GenJetsCustom_*_*', 'keep *_categorizeGenTtbar_*_*']),
     SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring("boosted_skimmed")
+        SelectEvents = cms.vstring("skimmed")
     )
 )
 process.endpath = cms.EndPath(process.OUT)
---
+++ crab/Summer18/template_cfg.py
@@ -0,0 +1,27 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'THEREQUESTNAME'
+config.General.workArea = 'crab_skims'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/nfs/dust/cms/user/mwassmer/ttH_2018/CMSSW_9_4_3/src/BoostedTTH/BoostedAnalyzer/test/skim.py'
+config.JobType.outputFiles = ['Skim.root']
+config.JobType.maxJobRuntimeMin = 1440
+config.JobType.maxMemoryMB = 2500 
+config.JobType.pyCfgParams = ['isData=False','maxEvents=99999999']
+config.JobType.sendPythonFolder=True
+
+config.Data.inputDataset = 'THEINPUTDATASET'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 2
+config.Data.publication = True
+config.Data.publishDBS = 'phys03'
+config.Data.outputDatasetTag = 'KIT_tthbb_sl_skims_v1'
+
+config.User.voGroup = 'dcms'
+
+config.Site.storageSite = 'T2_DE_DESY'
+#config.Site.blacklist = ['T2_US_*']
+#config.Site.whitelist = ['T2_DE_DESY']
