fbf9dda6c035d8882b85fec53feca40c5ca67a74
sebwieland <wielandseb@googlemail.com>
2018-08-29 11:34:24 +0200

    removed old comment

--- BoostedAnalyzer/src/SelectionTagProcessor.cpp
+++ BoostedAnalyzer/src/SelectionTagProcessor.cpp
@@ -21,7 +21,6 @@ void SelectionTagProcessor::Init(const InputCollections& input, VariableContaine
 void SelectionTagProcessor::Process(const InputCollections& input, VariableContainer& vars) {
 	if (!initialized) cerr << "tree processor not initialized" << endl;
 	recoSelected = true;
-	// genSelected = true;
 
 	for (auto& map : input.selectionTags) {
 		vars.FillVar( map.first, int(map.second));
