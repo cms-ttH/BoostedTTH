7ec485cad08a52f5d5c80838f01ab2ca7475fda9
Michael Wassmer <michael.wassmer@cern.ch>
2018-08-30 17:09:06 +0200

    path to new BDT weights to avoid crash with updated commonclassifier

--- BoostedAnalyzer/plugins/BoostedAnalyzer.cc
+++ BoostedAnalyzer/plugins/BoostedAnalyzer.cc
@@ -451,7 +451,7 @@ BoostedAnalyzer::BoostedAnalyzer(const edm::ParameterSet& iConfig): \
     jet_tag_pos = find (selectionNames.begin(), selectionNames.end(), "JetTagSelection") - selectionNames.begin();
     
     pointerToMEMClassifier.reset(new MEMClassifier());
-    pointerToCommonBDT5Classifier.reset(new BDTClassifier(string(getenv("CMSSW_BASE"))+"/src/TTH/CommonClassifier/data/bdtweights_Spring17V3/"));
+    pointerToCommonBDT5Classifier.reset(new BDTClassifier(string(getenv("CMSSW_BASE"))+"/src/TTH/CommonClassifier/data/lj_BDT_DeepCSV_Summer18_v1/"));
     DNNClassifierBase::pyInitialize();
     pointerToDnnSLClassifier.reset(new DNNClassifier_SL("v6a"));
     
