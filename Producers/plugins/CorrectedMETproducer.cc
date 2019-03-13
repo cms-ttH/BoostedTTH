11e5ef449c180fe844b3f3059388853d9cfa8be6
Michael Wassmer <michael.wassmer@cern.ch>
2019-03-13 10:16:51 +0100

    removed MiniAODHelper dependency from CorrectedMETproducer

--- Producers/plugins/CorrectedMETproducer.cc
+++ Producers/plugins/CorrectedMETproducer.cc
@@ -13,8 +13,9 @@
 
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/PatCandidates/interface/MET.h"
-
-#include "MiniAOD/MiniAODHelper/interface/MiniAODHelper.h"
+#include "DataFormats/PatCandidates/interface/Lepton.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
 //
 // class declaration
 //
@@ -25,6 +26,8 @@ public:
     ~CorrectedMETproducer();
     
     static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+    
+    std::vector<pat::MET> CorrectMET(const std::vector<pat::Jet>& oldJetsForMET, const std::vector<pat::Jet>& newJetsForMET, const std::vector<pat::Electron>& oldElectronsForMET, const std::vector<pat::Electron>& newElectronsForMET, const std::vector<pat::Muon>& oldMuonsForMET, const std::vector<pat::Muon>& newMuonsForMET, const std::vector<pat::MET>& pfMETs) const;
     
 private:
     virtual void beginStream(edm::StreamID) override;
@@ -46,10 +49,8 @@ private:
     edm::EDGetTokenT< std::vector<pat::Muon> > newMuonsToken;    
     /** mets data access token **/
     edm::EDGetTokenT< std::vector<pat::MET> > metsToken;
-    /** MiniAODHelper, used for jet correction and selection **/
-    MiniAODHelper helper;
     /** names of output jet collections **/
-    std::string collectionName;
+    const std::string collectionName;
     
 };
 
@@ -65,26 +66,18 @@ private:
 //
 // constructors and destructor
 //
-CorrectedMETproducer::CorrectedMETproducer(const edm::ParameterSet& iConfig)
+CorrectedMETproducer::CorrectedMETproducer(const edm::ParameterSet& iConfig) :  oldJetsToken{consumes< std::vector<pat::Jet> >(iConfig.getParameter<edm::InputTag>("oldJets"))},
+                                                                                newJetsToken{consumes< std::vector<pat::Jet> >(iConfig.getParameter<edm::InputTag>("newJets"))},
+                                                                                
+                                                                                oldElectronsToken{consumes< std::vector<pat::Electron> >(iConfig.getParameter<edm::InputTag>("oldElectrons"))},
+                                                                                newElectronsToken{consumes< std::vector<pat::Electron> >(iConfig.getParameter<edm::InputTag>("newElectrons"))},
+                                                                                
+                                                                                oldMuonsToken{consumes< std::vector<pat::Muon> >(iConfig.getParameter<edm::InputTag>("oldMuons"))},
+                                                                                newMuonsToken{consumes< std::vector<pat::Muon> >(iConfig.getParameter<edm::InputTag>("newMuons"))},
+                                                                                
+                                                                                metsToken{consumes< std::vector<pat::MET> >(iConfig.getParameter<edm::InputTag>("mets"))},
+                                                                                collectionName{iConfig.getParameter< std::string>("collectionName")}
 {
-    oldJetsToken  = consumes< std::vector<pat::Jet> >(iConfig.getParameter<edm::InputTag>("oldJets"));
-    newJetsToken  = consumes< std::vector<pat::Jet> >(iConfig.getParameter<edm::InputTag>("newJets"));
-    
-    oldElectronsToken  = consumes< std::vector<pat::Electron> >(iConfig.getParameter<edm::InputTag>("oldElectrons"));
-    newElectronsToken  = consumes< std::vector<pat::Electron> >(iConfig.getParameter<edm::InputTag>("newElectrons"));
-    
-    oldMuonsToken  = consumes< std::vector<pat::Muon> >(iConfig.getParameter<edm::InputTag>("oldMuons"));
-    newMuonsToken  = consumes< std::vector<pat::Muon> >(iConfig.getParameter<edm::InputTag>("newMuons"));
-    
-    metsToken  = consumes< std::vector<pat::MET> >(iConfig.getParameter<edm::InputTag>("mets"));
-    collectionName = iConfig.getParameter< std::string>("collectionName");
-
-    const bool isData = iConfig.getParameter<bool>("isData");
-    analysisType::analysisType iAnalysisType = analysisType::LJ;
-    const int sampleID = isData? -1 : 1;
-    const std::string era = "2015_74x";
-    helper.SetUp(era, sampleID, iAnalysisType, isData);
-
     produces<pat::METCollection>(collectionName);      
 }
 
@@ -126,10 +119,62 @@ CorrectedMETproducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
    edm::Handle< pat::METCollection > h_inputMETs;
    iEvent.getByToken( metsToken,h_inputMETs );
 
-   std::unique_ptr<pat::METCollection> correctedMETs = std::make_unique<pat::METCollection>(helper.CorrectMET(*h_oldJets,*h_newJets,*h_oldElectrons,*h_newElectrons,*h_oldMuons,*h_newMuons,*h_inputMETs));
+   std::unique_ptr<pat::METCollection> correctedMETs = std::make_unique<pat::METCollection>(CorrectMET(*h_oldJets,*h_newJets,*h_oldElectrons,*h_newElectrons,*h_oldMuons,*h_newMuons,*h_inputMETs));
    iEvent.put(std::move(correctedMETs),collectionName);
    
    
+}
+
+std::vector<pat::MET> CorrectedMETproducer::CorrectMET(const std::vector<pat::Jet>& oldJetsForMET, const std::vector<pat::Jet>& newJetsForMET, const std::vector<pat::Electron>& oldElectronsForMET, const std::vector<pat::Electron>& newElectronsForMET, const std::vector<pat::Muon>& oldMuonsForMET, const std::vector<pat::Muon>& newMuonsForMET , const std::vector<pat::MET>& pfMETs) const{
+  // this function takes two jet collections and replaces their contribution to the Type1 correction of the MET
+
+  std::vector<pat::MET> outputMets;
+
+  for(std::vector<pat::MET>::const_iterator oldMET=pfMETs.begin();oldMET!=pfMETs.end();++oldMET){
+    pat::MET outMET=*oldMET;
+    //cout << "before: uncor pt " << outMET.uncorPt() << " cor pt (type1) " << outMET.corPt(pat::MET::Type1) << " cor pt (type1XY) " << outMET.corPt(pat::MET::Type1XY) << endl;
+    if(oldMET-pfMETs.begin() == 0){
+    //get old MET p4
+    reco::Candidate::LorentzVector oldMETVec(oldMET->corPx(pat::MET::Type1),oldMET->corPy(pat::MET::Type1),oldMET->pz(),oldMET->corSumEt(pat::MET::Type1));
+    // add the pT vector of the old jets with the initial correction to the MET vector
+    for(std::vector<pat::Jet>::const_iterator itJet=oldJetsForMET.begin();itJet!=oldJetsForMET.end();++itJet){
+      reco::Candidate::LorentzVector PToldJETVec(itJet->px(),itJet->py(),0.,itJet->et());
+      oldMETVec=oldMETVec+PToldJETVec;
+    }   
+    // add the pT vector of the old electrons with the initial correction to the MET vector
+    for(std::vector<pat::Electron>::const_iterator itEle=oldElectronsForMET.begin();itEle!=oldElectronsForMET.end();++itEle){
+      reco::Candidate::LorentzVector PToldEleVec(itEle->px(),itEle->py(),0.,itEle->et());
+      oldMETVec=oldMETVec+PToldEleVec;
+    }
+    // add the pT vector of the old muonns with the initial correction to the MET vector
+    for(std::vector<pat::Muon>::const_iterator itMu=oldMuonsForMET.begin();itMu!=oldMuonsForMET.end();++itMu){
+      reco::Candidate::LorentzVector PToldMuVec(itMu->px(),itMu->py(),0.,itMu->et());
+      oldMETVec=oldMETVec+PToldMuVec;
+    }
+    // now subtract the pT vectors of the clean recorrected jets
+    for(std::vector<pat::Jet>::const_iterator itJet=newJetsForMET.begin();itJet!=newJetsForMET.end();++itJet){
+      reco::Candidate::LorentzVector PTnewJETVec(itJet->px(),itJet->py(),0.,itJet->et());
+      oldMETVec=oldMETVec-PTnewJETVec;
+    }
+    // now subtract the pT vectors of the recorrected electrons
+    for(std::vector<pat::Electron>::const_iterator itEle=newElectronsForMET.begin();itEle!=newElectronsForMET.end();++itEle){
+      reco::Candidate::LorentzVector PTnewEleVec(itEle->px(),itEle->py(),0.,itEle->et());
+      oldMETVec=oldMETVec-PTnewEleVec;
+    }
+    // now subtract the pT vectors of the recorrected muons
+    for(std::vector<pat::Muon>::const_iterator itMu=newMuonsForMET.begin();itMu!=newMuonsForMET.end();++itMu){
+      reco::Candidate::LorentzVector PTnewMuVec(itMu->px(),itMu->py(),0.,itMu->et());
+      oldMETVec=oldMETVec-PTnewMuVec;
+    }
+    outMET.setP4(oldMETVec);
+    //cout << "after: uncor pt " << outMET.uncorPt() << " cor pt (type1) " << outMET.corPt(pat::MET::Type1) << " cor pt (type1XY) " << outMET.corPt(pat::MET::Type1XY) << endl;
+    }
+
+    outputMets.push_back(outMET);
+  }
+
+  return outputMets;
+  
 }
 
 // ------------ method called once each stream before processing any runs, lumis or events  ------------
