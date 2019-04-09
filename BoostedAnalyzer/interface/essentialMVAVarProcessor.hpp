b56a5a0d89e24b78fde7430e7512d440440697cf
sebwieland <wielandseb@googlemail.com>
2019-04-09 15:55:57 +0200

    removed deprecated constructor

--- BoostedAnalyzer/interface/essentialMVAVarProcessor.hpp
+++ BoostedAnalyzer/interface/essentialMVAVarProcessor.hpp
@@ -15,7 +15,6 @@ class essentialMVAVarProcessor : public TreeProcessor
 
 public:
   essentialMVAVarProcessor();
-  essentialMVAVarProcessor(MEMClassifier *mem_, MVAvars *mvavars);
 
   ~essentialMVAVarProcessor();
 
