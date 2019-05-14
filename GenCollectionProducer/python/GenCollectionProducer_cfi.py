ec7e7b8617d7a9c5b2e262f3a75aa19bd535f3ef
Michael Wassmer <michael.wassmer@cern.ch>
2019-05-14 12:49:27 +0200

    Added GenCollectionProducer again. Useful package for particle level studies.

---
+++ GenCollectionProducer/python/GenCollectionProducer_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+GenCollectionProducer = cms.EDProducer('GenCollectionProducer',
+                      collection_name = cms.vstring("CustomGenElectrons","CustomGenMuons","CustomGenTaus","CustomGenPhotons","CustomGenJets","CustomGenJetsAK8"),
+                      collection_type = cms.vstring("Electron","Muon","Tau","Photon","AK4Jet","AK8Jet"),
+                      pt_min = cms.vdouble(10.,10.,10.,10.,10.),
+                      eta_max = cms.vdouble(2.4,2.4,2.4,2.4,2.4),
+                      genParticles=cms.InputTag("prunedGenParticles"),
+                      genJets=cms.InputTag("slimmedGenJets"),
+                      genJetsAK8=cms.InputTag("slimmedGenJetsAK8"),
+                      doDeltaRCleaning = cms.bool(False)
+)
