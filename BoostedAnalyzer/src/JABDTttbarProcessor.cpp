a4c1cec52e0fc00a6f969e4d86e824fc7608336b
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2019-06-05 10:47:27 +0200

    weightpath is now built relativ to CMSSW base

--- BoostedAnalyzer/src/JABDTttbarProcessor.cpp
+++ BoostedAnalyzer/src/JABDTttbarProcessor.cpp
@@ -7,18 +7,20 @@ JABDTttbarProcessor::JABDTttbarProcessor(const edm::ParameterSet& iConfig)
       const edm::ParameterSet jaoptions = iConfig.getParameter<edm::ParameterSet>("JetAssignmentOptions");
       std::string weightpath = "";
       try{
-        weightpath = jaoptions.getParameter<std::string>("ttbar_weightpath");
+        weightpath = std::string(getenv("CMSSW_BASE")) + "/src/" +jaoptions.getParameter<std::string>("ttbar_weightpath");
       }
       catch(const std::exception& e){
           std::cerr << "ERROR: variable 'ttbar_weightpath' does not exist in ParameterSet 'JetAssignentOptions'\n";
           std::cerr << e.what() << std::endl;
           return;
       }
-
+      std::cout << "INIT OF JABDTttbarProcessor\n";
+      std::cout << "FOUND THIS WEIGHTFILE: " << weightpath << std::endl;
       std::string optional_varstring = "";
       if (jaoptions.existsAs<std::string>("ttbar_varlist", true)) optional_varstring = jaoptions.getParameter<std::string>("ttbar_varlist");
-      
+      std::cout << "INTIIALIZING 'ttbarHypothesisCombinatorics\n";
       pointerToHypothesisCombinatorics.reset(new ttbarHypothesisCombinatorics(weightpath, optional_varstring));
+      // std::cout << "TYPE: " << typeid(pointerToHypothesisCombinatorics).name << std::endl;
       if( iConfig.existsAs<std::string>("ttbar_prefix",true) ) hypothesis = iConfig.getParameter<std::string>("ttbar_prefix");
       else hypothesis = "ttbar";
   }
