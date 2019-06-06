701125de2af62ee2e281d107de8aceac743c7d36
Jan van der Linden <jan.linden@student.kit.edu>
2019-06-06 10:25:46 +0200

    increase tag selection to >=3 tags for ttZ analysis

--- BoostedAnalyzer/python/Selection_cff.py
+++ BoostedAnalyzer/python/Selection_cff.py
@@ -80,7 +80,7 @@ DiLeptonMETSelection = cms.PSet(
 JetTagSelection = cms.PSet(
     minJets = cms.vint32(4),
     maxJets = cms.vint32(-1),
-    minTags = cms.vint32(2),
+    minTags = cms.vint32(3),
     maxTags = cms.vint32(-1)
 )
 
