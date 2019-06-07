f84bd680d3c6ad11ba6be67aa25371df3ac10b0a
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2019-06-07 14:00:04 +0200

    implemented input files for different hypotheses

--- BoostedAnalyzer/python/JetAssignment_cff.py
+++ BoostedAnalyzer/python/JetAssignment_cff.py
@@ -1,9 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-# PU weights
-# ------------------------------------------------------------
-
-#2018
+#2017
 JetAssignment2017 = cms.PSet(
-    ttbar_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/BDTrecoTTBar_expert_BDT.weights.xml")
+    ttbar_even_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/ttbar_even_weights.xml"),
+    ttbar_odd_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/ttbar_odd_weights.xml"),
+    ttbar_varlist = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/ttbar_variables.csv"),
+
+    # ttH_even_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/ttbar_even_weights.xml"),
+    # ttH_odd_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/ttbar_odd_weights.xml"),
+    # ttH_varlist = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/tth_variables.csv"),
+
+    thq_even_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/tHq_even_weights.xml"),
+    thq_odd_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/tHq_odd_weights.xml"),
+    thq_varlist = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/tHq_variables.csv"),
+
+    thw_even_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/tHW_even_weights.xml"),
+    thw_odd_weightpath = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/tHW_odd_weights.xml"),
+    thw_varlist = cms.string("BoostedTTH/BoostedAnalyzer/data/bdtweights/tHW_variables.csv"),
+
 )
+JetAssignment2016 = JetAssignment2017.clone()
+JetAssignment2018 = JetAssignment2017.clone()
