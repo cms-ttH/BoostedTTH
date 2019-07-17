7696bc39271581fe0faac317da3bac28b4d80c42
Michael Wassmer <michael.wassmer@cern.ch>
2019-07-17 11:06:24 +0200

    do not save __init__.py files since they are generated by scram automatically

--- BoostedAnalyzer/python/__init__.py
+++
@@ -1,9 +0,0 @@
-# Automatically created by SCRAM
-import os
-
-__path__.append(
-    os.path.dirname(
-        os.path.abspath(__file__).rsplit("/BoostedTTH/BoostedAnalyzer/", 1)[0]
-    )
-    + "/cfipython/slc6_amd64_gcc530/BoostedTTH/BoostedAnalyzer"
-)
--- GenCollectionProducer/python/__init__.py
+++
@@ -1,9 +0,0 @@
-# Automatically created by SCRAM
-import os
-
-__path__.append(
-    os.path.dirname(
-        os.path.abspath(__file__).rsplit("/BoostedTTH/GenCollectionProducer/", 1)[0]
-    )
-    + "/cfipython/slc6_amd64_gcc530/BoostedTTH/GenCollectionProducer"
-)
