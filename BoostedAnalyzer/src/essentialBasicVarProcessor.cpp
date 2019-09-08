8ee2bbf16545e454e88ae24de24819a1c808c926
Michael Wassmer <michael.wassmer@cern.ch>
2019-09-08 16:21:58 +0200

    added HEM ak4 jets

--- BoostedAnalyzer/src/essentialBasicVarProcessor.cpp
+++ BoostedAnalyzer/src/essentialBasicVarProcessor.cpp
@@ -100,6 +100,9 @@ void essentialBasicVarProcessor::Init(const InputCollections& input, VariableCon
     vars.InitVar("Gen_chsMET_Phi");
 
     vars.InitVars("CSV", "N_Jets");
+    
+    vars.InitVar("N_HEM_Jets", "I");
+    
     initialized = true;
 }
 
@@ -137,6 +140,8 @@ void essentialBasicVarProcessor::Process(const InputCollections& input, Variable
     vars.FillVar("N_PrimaryVertices", input.selectedPVs.size());
     vars.FillVar("N_Jets", input.selectedJets.size());
     //     vars.FillVar("N_LooseJets", input.selectedJetsLoose.size());
+
+    int N_HEM_Jets = 0;
 
     // Fill Jet Variables
     // All Jets
@@ -176,6 +181,8 @@ void essentialBasicVarProcessor::Process(const InputCollections& input, Variable
         vars.FillVars("Jet_DeepJet_c", iJet, CSVHelper::GetJetCSV_DNN(*itJet, "pfDeepFlavourJetTags:probc"));
         vars.FillVars("Jet_DeepJet_uds", iJet, CSVHelper::GetJetCSV_DNN(*itJet, "pfDeepFlavourJetTags:probuds"));
         vars.FillVars("Jet_DeepJet_g", iJet, CSVHelper::GetJetCSV_DNN(*itJet, "pfDeepFlavourJetTags:probg"));
+        
+        if(itJet->eta()<-1.4 && itJet->eta()>-3.0 && itJet->phi()<-0.87 && itJet->phi()>-1.57) N_HEM_Jets+=1;
     }
 
     //     for (std::vector< pat::Jet >::const_iterator itJet = input.selectedJetsLoose.begin(); itJet != input.selectedJetsLoose.end(); ++itJet) {
@@ -265,4 +272,6 @@ void essentialBasicVarProcessor::Process(const InputCollections& input, Variable
         int iCSV = itCSV - csvJetsSorted.begin();
         vars.FillVars("CSV", iCSV, *itCSV);
     }
+    
+    vars.FillVar("N_HEM_Jets",N_HEM_Jets);
 }
