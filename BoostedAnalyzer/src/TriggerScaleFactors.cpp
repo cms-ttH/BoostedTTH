aa740e91c0abcfc0be0ae95f5d43ede89d53f70d
Michael Wassmer <michael.wassmer@cern.ch>
2019-09-16 12:53:53 +0200

    more comments for TriggerScaleFactors calss

--- BoostedAnalyzer/src/TriggerScaleFactors.cpp
+++ BoostedAnalyzer/src/TriggerScaleFactors.cpp
@@ -1,17 +1,24 @@
 #include "BoostedTTH/BoostedAnalyzer/interface/TriggerScaleFactors.hpp"
 
+// constructor sets initiliazed flag to false and calls Init() function which should do all necessary things
+// before the scale factors can be calculated, e.g. open files and so on ...
 TriggerScaleFactors::TriggerScaleFactors(std::string name) : initialized{false}, name{name} { Init(); }
 
+// destructor should also be implemented in derived classes
 TriggerScaleFactors::~TriggerScaleFactors() {}
 
+// Init function should be implemented in derived classes and overwrite this base function
 void TriggerScaleFactors::Init() { initialized = true; }
 
+// base function to calculate trigger scale factors
+// if the derived class does not overwrite this function, only three 1s will be returned
 std::vector< float > TriggerScaleFactors::CalculateTriggerSFs(const InputCollections& input)
 {
     std::vector< float > ones{1.0, 1.0, 1.0};
     return ones;
 }
 
+// function which returns the correct trigger scale factors if the class was initialized
 std::vector< float > TriggerScaleFactors::GetTriggerSFs(const InputCollections& input)
 {
     if (initialized)
