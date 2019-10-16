123bc8143a8c67783f2e031b911a5b5106c7beda
sebwieland <wielandseb@googlemail.com>
2019-10-16 09:39:39 +0200

    hacked custom 20 % JES uncertainty for HEM jets for extra JES source

--- Producers/plugins/SelectedJetProducer.cc
+++ Producers/plugins/SelectedJetProducer.cc
@@ -574,18 +574,6 @@ pat::Jet SelectedJetProducer::GetCorrectedJet(const pat::Jet &inputJet, const ed
   pat::Jet outputJet = inputJet;
   bool addUserFloats = true;
 
-  if(era.find("2018") != std::string::npos)
-  {
-    bool isHEM = (-3 < outputJet.eta()) && (outputJet.eta() < -1.3);
-    isHEM = isHEM && ( (-1.57 < outputJet.phi()) && (outputJet.phi() < -0.87) );
-    if (isHEM) 
-    {
-      uncFactor_ = 1.2;
-    }
-  }
-  else{
-    uncFactor_ = 1.0;
-  }
   ApplyJetEnergyCorrection(outputJet, factor, event, setup, genjets, iSysType, doJES, doJER, addUserFloats, corrFactor, uncFactor_);
 
   return outputJet;
@@ -613,11 +601,21 @@ void SelectedJetProducer::ApplyJetEnergyCorrection(pat::Jet &jet, double &totalC
       {
         edm::LogError("SelectedJetProducer") << "Trying to use GetCorrectedJets() without setting jet corrector!";
       }
+      
+      // Figure out if HEM issue -> only for 2018 
+      bool isHEM = false;
+      if(era.find("2018") != std::string::npos)
+      {
+        isHEM = (-3 < jet.eta()) && (jet.eta() < -1.3);
+        isHEM = isHEM && ( (-1.57 < jet.phi()) && (jet.phi() < -0.87) );
+      }
 
       const double jec = scale * corrFactor;
       jet.scaleEnergy(jec);
       totalCorrFactor *= jec;
+      
 
+      
       if (addUserFloats)
       {
         jet.addUserFloat("HelperJES", scale);
@@ -631,7 +629,29 @@ void SelectedJetProducer::ApplyJetEnergyCorrection(pat::Jet &jet, double &totalC
 
       if (SystematicsHelper::isJECUncertainty(iSysType))
       {
-        const double unc = GetJECUncertainty(jet, setup, iSysType);
+        double unc = 1.0;
+        if (iSysType == SystematicsHelper::Type::JESHEMup)
+        {
+          if (isHEM){
+            unc = 0.2;
+            // std::cout << "DEBUG got HEM JET setting unc to 0.2" << std::endl;
+          } 
+          else unc = 0.0;
+        }
+        else if (iSysType == SystematicsHelper::Type::JESHEMdown)
+        {
+          if (isHEM){
+            unc = -0.2;
+            // std::cout << "DEBUG got HEM JET setting unc to -0.2" << std::endl;
+          } 
+          else unc = 0.0;
+        }
+        else
+        {
+          // std::cout << "DEBUG got standard JES setting unc to whatever" << std::endl;
+          unc = GetJECUncertainty(jet, setup, iSysType);
+        }
+        // std::cout << "DEBUG: unc = " << unc << std::endl;
         const double jecvar = 1. + (unc * uncFactor);
         if (addUserFloats)
         {
