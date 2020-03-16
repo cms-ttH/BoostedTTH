a8fc222f1916914af444404565ef088cb124760e
sebwieland <wielandseb@googlemail.com>
2020-03-16 15:35:14 +0100

    increase unitsperJob

--- crab/common/template_cfg_ntuple.py
+++ crab/common/template_cfg_ntuple.py
@@ -9,13 +9,13 @@ config.JobType.psetName = 'CMSSWPATH/RUNCONFIG'
 config.JobType.outputFiles = OUTPUTFILES
 # config.JobType.maxJobRuntimeMin = 2800
 config.JobType.maxMemoryMB = 4000 
-config.JobType.pyCfgParams = ['isData=ISDATA','maxEvents=99999999','outName=OUTNAME', 'dataEra=DATAERA','systematicVariations=SYSTEMATICVARIATIONS','weight=WEIGHT','ProduceMemNtuples=SLIMMED', 'deterministicSeeds=SEEDS']
+config.JobType.pyCfgParams = ['isData=ISDATA','maxEvents=999999999','outName=OUTNAME', 'dataEra=DATAERA','systematicVariations=SYSTEMATICVARIATIONS','weight=WEIGHT','ProduceMemNtuples=SLIMMED', 'deterministicSeeds=SEEDS']
 config.JobType.sendPythonFolder=True
 
 config.Data.inputDataset = 'THEINPUTDATASET'
 config.Data.inputDBS = 'DBSINSTANCE'
 # config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 600
+config.Data.unitsPerJob = 800
 config.Data.splitting = 'Automatic'
 config.Data.publication = False
 config.Data.publishDBS = 'phys03'
