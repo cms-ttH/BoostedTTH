30aa156b550c4612ce34b18969366c4e0338e7d4
Michael Wassmer <michael.wassmer@cern.ch>
2020-03-24 17:08:40 +0100

    use electron scale factors from monojet group

--- BoostedAnalyzer/src/SingleElTriggerSFs.cpp
+++ BoostedAnalyzer/src/SingleElTriggerSFs.cpp
@@ -14,16 +14,16 @@ SingleElTriggerSFs::~SingleElTriggerSFs()
 void SingleElTriggerSFs::Init(const InputCollections& input)
 {
     if (input.era.find("2016") != std::string::npos) {
-        file = TString(std::string(getenv("CMSSW_BASE"))) + "/src/MiniAOD/MiniAODHelper/data/Run2Legacy/SingleEG_JetHT_Trigger_Scale_Factors_ttHbb2016_v3.root";
-        hist_name = "ele27_ele_pt_ele_sceta";
+        file = TString(std::string(getenv("CMSSW_BASE"))) + "/src/MiniAOD/MiniAODHelper/data/Run2Legacy/electron_trigger_sf_2017.root";
+        hist_name = "EGamma_SF2D";
     }
     else if (input.era.find("2017") != std::string::npos) {
-        file = TString(std::string(getenv("CMSSW_BASE"))) + "/src/MiniAOD/MiniAODHelper/data/Run2Legacy/SingleEG_JetHT_Trigger_Scale_Factors_ttHbb2017_v3.root";
-        hist_name = "ele28_ht150_OR_ele35_ele_pt_ele_sceta";
+        file = TString(std::string(getenv("CMSSW_BASE"))) + "/src/MiniAOD/MiniAODHelper/data/Run2Legacy/electron_trigger_sf_2017.root";
+        hist_name = "EGamma_SF2D";
     }
     else if (input.era.find("2018") != std::string::npos) {
-        file = TString(std::string(getenv("CMSSW_BASE"))) + "/src/MiniAOD/MiniAODHelper/data/Run2Legacy/SingleEG_JetHT_Trigger_Scale_Factors_ttHbb2018_v3.root";
-        hist_name = "ele28_ht150_OR_ele32_ele_pt_ele_sceta";
+        file = TString(std::string(getenv("CMSSW_BASE"))) + "/src/MiniAOD/MiniAODHelper/data/Run2Legacy/electron_trigger_sf_2018.root";
+        hist_name = "EGamma_SF2D";
     }
     else {
         initialized = false;
@@ -64,18 +64,18 @@ std::vector< float > SingleElTriggerSFs::CalculateTriggerSFs(const InputCollecti
     eta = -999.;
 
     // make sure to stay within the range ot the histograms
-    pt  = std::max(xmin + 0.1, input.selectedElectronsLoose.at(0).pt());
-    pt  = std::min(float(xmax - 0.1), float(pt));
-    eta = std::max(ymin + 0.1, input.selectedElectronsLoose.at(0).superCluster().isAvailable()
+    pt  = std::max(ymin + 0.1, input.selectedElectronsLoose.at(0).pt());
+    pt  = std::min(float(ymax - 0.1), float(pt));
+    eta = std::max(xmin + 0.1, input.selectedElectronsLoose.at(0).superCluster().isAvailable()
                                    ? input.selectedElectronsLoose.at(0).superCluster()->position().eta()
                                    : input.selectedElectronsLoose.at(0).eta());
-    eta = std::min(float(ymax - 0.1), float(eta));
+    eta = std::min(float(xmax - 0.1), float(eta));
     // std::cout << "Pt: " << pt << std::endl;
     // std::cout << "Eta: " << eta << std::endl;
     // calculate SFs here
-    sfs.at(0) = hist->GetBinContent(hist->FindBin(pt, eta));
-    sfs.at(1) = (hist->GetBinContent(hist->FindBin(pt, eta))) + (hist->GetBinError(hist->FindBin(pt, eta)));
-    sfs.at(2) = (hist->GetBinContent(hist->FindBin(pt, eta))) - (hist->GetBinError(hist->FindBin(pt, eta)));
+    sfs.at(0) = hist->GetBinContent(hist->FindBin(eta, pt));
+    sfs.at(1) = (hist->GetBinContent(hist->FindBin(eta, pt))) + (hist->GetBinError(hist->FindBin(eta, pt)));
+    sfs.at(2) = (hist->GetBinContent(hist->FindBin(eta, pt))) - (hist->GetBinError(hist->FindBin(eta, pt)));
 
     return sfs;
 }
